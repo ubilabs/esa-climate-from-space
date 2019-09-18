01882e8162b3796e09f4c883dd8dcbcc2db22e69
Katherina Marcenko <45818654+KatvonRivia@users.noreply.github.com>
2019-09-18 17:38:28 +0200

    feat(eslint): add eslint (#5)

--- src/scripts/components/app/app.tsx
+++ src/scripts/components/app/app.tsx
@@ -1,8 +1,6 @@
 import React, {FunctionComponent} from 'react';
 import styles from './app.styl';
 
-const App: FunctionComponent<{}> = () => {
-  return <h1 className={styles.app}>Hello</h1>;
-};
+const App: FunctionComponent<{}> = () => <h1 className={styles.app}>Hello</h1>;
 
 export default App;
