a0bfe5af211936ad8e79f9dde9f0a1700b358940
Katherina Marcenko <marcenko@ubilabs.net>
2019-10-17 16:42:42 +0200

    feat(story): add story sidepanel

--- src/scripts/components/story-item/story-item.tsx
+++ src/scripts/components/story-item/story-item.tsx
@@ -1,18 +1,20 @@
 import React, {FunctionComponent} from 'react';
 
 import {Story} from '../../types/story';
+import {Link} from 'react-router-dom';
 
 import styles from './story-item.styl';
 
 interface Props {
   story: Story;
 }
-
 const StoryItem: FunctionComponent<Props> = ({story}) => (
-  <div className={styles.storyItem}>
-    <div className={styles.image}></div>
-    <p className={styles.title}>{story.title}</p>
-  </div>
+  <Link to={`/stories/${story.id}`}>
+    <div className={styles.storyItem}>
+      <div className={styles.image}></div>
+      <p className={styles.title}>{story.title}</p>
+    </div>
+  </Link>
 );
 
 export default StoryItem;
