76ac0c6ee43cce9c6e373092cff6bfff766a9cf5
Katherina Marcenko <marcenko@ubilabs.net>
2019-10-21 18:46:08 +0200

    feat(story): display story data in sidepanel

--- src/scripts/components/story/story.tsx
+++ src/scripts/components/story/story.tsx
@@ -1,12 +1,14 @@
 import React, {FunctionComponent, useEffect} from 'react';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {useParams} from 'react-router-dom';
 
 import fetchStory from '../../actions/fetch-story';
+import {storySelector} from '../../reducers/story';
 
 import styles from './story.styl';
 
 const Story: FunctionComponent = () => {
+  const story = useSelector(storySelector);
   const dispatch = useDispatch();
   const {storyId} = useParams();
 
@@ -16,18 +18,18 @@ const Story: FunctionComponent = () => {
 
   return (
     <div className={styles.story}>
-      <div className={styles.sidepanel}>
-        <div className={styles.previewImage}></div>
-        <div className={styles.content}>
-          <h1>Test</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor optio
-            nisi nobis quas ut. Exercitationem, sapiente. Praesentium quidem
-            mollitia explicabo voluptatem aperiam deleniti ut sunt atque eaque,
-            voluptate commodi in.
-          </p>
-        </div>
-      </div>
+      {story &&
+        story.slides.map(slide => {
+          return (
+            <div className={styles.sidepanel} key={slide.title}>
+              <img src={slide.image} className={styles.previewImage} />
+              <div className={styles.content}>
+                <h1>{slide.title}</h1>
+                <p>{slide.bodytext}</p>
+              </div>
+            </div>
+          );
+        })}
     </div>
   );
 };
