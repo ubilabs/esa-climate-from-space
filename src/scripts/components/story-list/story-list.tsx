41df51f4950d1607d5e11bd15145bc02a3367d4c
Katherina Marcenko <45818654+KatvonRivia@users.noreply.github.com>
2019-10-25 14:33:33 +0200

    feat(story-title): add story title (#175)
    * feat(story): add pagination
    
    * fix(icons): rename files
    
    * refactor(pagination): move pagination to own component
    
    * refactor(pagination): remove comment
    
    * feat(story): add story title
    
    * refactor(story-title): change variable name

--- src/scripts/components/story-list/story-list.tsx
+++ src/scripts/components/story-list/story-list.tsx
@@ -1,19 +1,13 @@
-import React, {FunctionComponent, useEffect} from 'react';
-import {useSelector, useDispatch} from 'react-redux';
+import React, {FunctionComponent} from 'react';
+import {useSelector} from 'react-redux';
 
 import {storiesSelector} from '../../reducers/stories';
-import fetchStories from '../../actions/fetch-stories';
 import StoryListItem from '../story-list-item/story-list-item';
 
 import styles from './story-list.styl';
 
 const StoryList: FunctionComponent = () => {
   const stories = useSelector(storiesSelector);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchStories());
-  }, [dispatch]);
 
   return (
     <div className={styles.storyList}>
