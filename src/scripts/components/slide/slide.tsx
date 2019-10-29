d7497edf9176a0207763cea0252e2905c2622396
Katherina Marcenko <marcenko@ubilabs.net>
2019-10-29 12:08:52 +0100

    feat(story-gallery): add gallery to story sidepanel

--- src/scripts/components/slide/slide.tsx
+++ src/scripts/components/slide/slide.tsx
@@ -1,6 +1,7 @@
 import React, {FunctionComponent} from 'react';
 import {Slide as SlideType} from '../../types/story';
 import styles from './slide.styl';
+import StoryGallery from '../story-gallery/story-gallery';
 
 interface Props {
   slide: SlideType;
@@ -8,7 +9,9 @@ interface Props {
 
 const Slide: FunctionComponent<Props> = ({slide}) => (
   <div className={styles.slide}>
-    <img src={slide.image} className={styles.previewImage} />
+    {slide.images && slide.images.length && (
+      <StoryGallery images={slide.images} />
+    )}
     <div className={styles.content}>
       <h1>{slide.title}</h1>
       <p>{slide.bodytext}</p>
---
+++ src/scripts/components/story-gallery/story-gallery.tsx
@@ -0,0 +1,60 @@
+import React, {FunctionComponent, useState} from 'react';
+import {BackIcon} from '../icons/back-icon';
+import {NextIcon} from '../icons/next-icon';
+
+import styles from './story-gallery.styl';
+
+interface Props {
+  images: [];
+}
+
+const StoryGallery: FunctionComponent<Props> = ({images}) => {
+  const imagesLength = images.length;
+  const containerWidth = imagesLength * 100;
+  const imageWidth = 100 / imagesLength;
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const showBackButton = currentIndex > 0;
+  const showNextButton = currentIndex < imagesLength - 1;
+
+  const onBackClick = () => {
+    if (currentIndex <= 0) {
+      return null;
+    }
+    return setCurrentIndex(currentIndex - 1);
+  };
+
+  const onNextClick = () => {
+    if (currentIndex >= imagesLength - 1) {
+      return null;
+    }
+    return setCurrentIndex(currentIndex + 1);
+  };
+
+  return (
+    <div className={styles.storyGallery}>
+      <div className={styles.buttonContainer}>
+        <div onClick={onBackClick}>{showBackButton ? <BackIcon /> : null}</div>
+        <div onClick={onNextClick}>{showNextButton ? <NextIcon /> : null}</div>
+      </div>
+      <div
+        className={styles.slider}
+        style={{
+          width: `${containerWidth}%`,
+          transform: `translateX(-${imageWidth * currentIndex}%)`,
+          transition: 'transform ease-out 0.50s'
+        }}>
+        {images.map((image, index) => (
+          <img
+            className={styles.sliderImage}
+            src={image}
+            alt="img"
+            key={index}
+            style={{width: `${imageWidth}%`}}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default StoryGallery;
