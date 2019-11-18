86153c89ca9986ee60b98225ced9cc5b96ce8dac
Katherina Marcenko <marcenko@ubilabs.net>
2019-11-18 15:53:13 +0100

    refactor(feat/presenter-mode): remove curly braces

--- src/scripts/components/presentation-selector/presentation-selector.tsx
+++ src/scripts/components/presentation-selector/presentation-selector.tsx
@@ -8,17 +8,16 @@ import {StoryMode} from '../../types/story-mode';
 
 import styles from './presentation-selector.styl';
 
-const PresentationSelector: FunctionComponent = () =>(
-    <div className={styles.presentationSelector}>
-      <Link to="/" className={styles.backButton}>
-        <FormattedMessage id="goBack" />
-      </Link>
-      <h1 className={styles.title}>
-        <FormattedMessage id="presenter-mode" />
-      </h1>
-      <StoryList mode={StoryMode.Present} />
-    </div>
-  );
+const PresentationSelector: FunctionComponent = () => (
+  <div className={styles.presentationSelector}>
+    <Link to="/" className={styles.backButton}>
+      <FormattedMessage id="goBack" />
+    </Link>
+    <h1 className={styles.title}>
+      <FormattedMessage id="presenter-mode" />
+    </h1>
+    <StoryList mode={StoryMode.Present} />
+  </div>
 );
 
 export default PresentationSelector;
