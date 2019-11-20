2174cbbc31a305ee82f664e4aefa6710a16eb91d
Katherina Marcenko <marcenko@ubilabs.net>
2019-11-20 14:03:22 +0100

    feat(presenter): add control panel

--- src/scripts/components/story-pagination/story-pagination.tsx
+++ src/scripts/components/story-pagination/story-pagination.tsx
@@ -1,17 +1,21 @@
 import React, {FunctionComponent} from 'react';
 import {Link} from 'react-router-dom';
+import cx from 'classnames';
 
 import {PreviousIcon} from '../icons/back-icon';
 import {NextIcon} from '../icons/next-icon';
+import {PlayIcon} from '../icons/play-icon';
 
 import {Slide} from '../../types/story';
+import {StoryMode} from '../../types/story-mode';
 
 import styles from './story-pagination.styl';
+import {RemoveIcon} from '../icons/remove-icon';
 
 interface Props {
   currentPage: number;
   storyId: string;
-  mode: string;
+  mode: StoryMode;
   slides: Slide[];
 }
 
@@ -27,29 +31,40 @@ const StoryPagination: FunctionComponent<Props> = ({
   const showNextButton = nextPageNumber < slidesLength;
   const showPreviousButton = previousPageNumber >= 0;
 
+  const presenterMode = mode === StoryMode.Present;
+
+  const classes = cx(styles.pagination, presenterMode && styles.present);
+
   return (
-    <div className={styles.pagination}>
-      {showPreviousButton ? (
-        <Link
-          to={`/${mode}/${storyId}/${previousPageNumber}`}
-          className={styles.icon}>
-          <PreviousIcon />
-        </Link>
-      ) : (
-        <div className={styles.emptyIcon} />
-      )}
-      <span>
-        {currentPage + 1}/{slidesLength}
-      </span>
-      {showNextButton ? (
-        <Link
-          to={`/${mode}/${storyId}/${nextPageNumber}`}
-          className={styles.icon}>
-          <NextIcon />
-        </Link>
-      ) : (
-        <div className={styles.emptyIcon} />
-      )}
+    <div className={classes}>
+      <div className={styles.controls}>
+        {showPreviousButton ? (
+          <Link
+            to={`/${mode}/${storyId}/${previousPageNumber}`}
+            className={styles.icon}>
+            <PreviousIcon />
+          </Link>
+        ) : (
+          <div className={styles.emptyIcon} />
+        )}
+        <span>
+          {currentPage + 1}/{slidesLength}
+        </span>
+        {showNextButton ? (
+          <Link
+            to={`/${mode}/${storyId}/${nextPageNumber}`}
+            className={styles.icon}>
+            <NextIcon />
+          </Link>
+        ) : (
+          <div className={styles.emptyIcon} />
+        )}
+        {presenterMode && (
+          <div className={styles.icons}>
+            <PlayIcon /> <RemoveIcon />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
