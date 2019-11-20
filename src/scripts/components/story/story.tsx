3ebb7aa254650b2c2ebf606682da7ad887afa66b
Katherina Marcenko <marcenko@ubilabs.net>
2019-11-20 10:13:50 +0100

    refactor(story): import defaultView from config

--- src/scripts/components/story/story.tsx
+++ src/scripts/components/story/story.tsx
@@ -11,6 +11,7 @@ import {storyListSelector} from '../../selectors/story/list';
 import setFlyToAction from '../../actions/set-fly-to';
 import Slide from '../slide/slide';
 import {State} from '../../reducers';
+import globeState from '../../config/main';
 
 import {StoryMode} from '../../types/story-mode';
 
@@ -30,18 +31,7 @@ const Story: FunctionComponent<Props> = ({mode}) => {
   const pageNumber = parseInt(page || '0', 10);
   const slide = story && story.slides[pageNumber];
   const storyListItem = stories.find(storyItem => storyItem.id === storyId);
-  const defaultView = {
-    position: {
-      height: 14484862,
-      latitude: 40.659017,
-      longitude: 0.002816
-    },
-    orientation: {
-      heading: 0,
-      pitch: -90,
-      roll: 0
-    }
-  };
+  const defaultView = globeState.globe.view;
 
   // fetch story of active storyId
   useEffect(() => {
