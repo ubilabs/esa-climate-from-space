10dfaa7838633c681528e8d358f2d43d86ac412b
Katherina Marcenko <marcenko@ubilabs.net>
2019-11-27 10:58:45 +0100

    refactor(showcase): add return in custom hook

--- src/scripts/hooks/use-interval.ts
+++ src/scripts/hooks/use-interval.ts
@@ -18,5 +18,7 @@ export const useInterval = (callback: () => void, delay: number) => {
       const id = setInterval(tick, delay);
       return () => clearInterval(id);
     }
+
+    return () => {};
   }, [delay]);
 };
