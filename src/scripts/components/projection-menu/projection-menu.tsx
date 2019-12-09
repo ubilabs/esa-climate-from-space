52c8f4b435913c14bdd0c6d6a7e3c9d1688240ce
Katherina Marcenko <marcenko@ubilabs.net>
2019-12-09 17:44:18 +0100

    refactor(story-globe): change morphTime

--- src/scripts/components/projection-menu/projection-menu.tsx
+++ src/scripts/components/projection-menu/projection-menu.tsx
@@ -16,7 +16,7 @@ const ProjectionMenu: FunctionComponent = () => {
   const [isOpen, setIsOpen] = useState(false);
   const onProjectionClick = (projection: GlobeProjection) => {
     setIsOpen(false);
-    dispatch(setGlobeProjectionAction(projection, 5));
+    dispatch(setGlobeProjectionAction(projection, 2));
   };
   const onButtonClickHandler = () => setIsOpen(!isOpen);
 
