188e8153d85eb55f30631d5fe7525040ceb7c6df
KatvonRivia <katvonrivia@gmail.com>
2020-02-24 18:15:18 +0100

    feat(menu): add menu to overlay

--- src/scripts/components/menu/menu.tsx
+++ src/scripts/components/menu/menu.tsx
@@ -1,6 +1,4 @@
-import React, {FunctionComponent, useState} from 'react';
-import {Link} from 'react-router-dom';
-import {useIntl, FormattedMessage} from 'react-intl';
+import React, {FunctionComponent} from 'react';
 
 import {PresenterIcon} from '../icons/presenter-icon';
 import {ShowCaseIcon} from '../icons/show-case-icon';
@@ -9,70 +7,59 @@ import {ShareIcon} from '../icons/share-icon';
 import {InfoIcon} from '../icons/info-icon';
 import {ExportIcon} from '../icons/export-icon';
 import LanguageSelector from '../language-selector/language-selector';
+import Button from '../button/button';
+import {CCILogo} from '../icons/cci-logo';
 
 import {MenuItem} from '../../types/menu-item';
 
 import styles from './menu.styl';
 
 const Menu: FunctionComponent = () => {
-  const intl = useIntl();
   const menuItems: MenuItem[] = [
     {
-      id: 'presenter-mode',
-      name: intl.formatMessage({id: 'presenterMode'}),
+      id: 'presenterMode',
       link: '/present',
       icon: PresenterIcon
     },
     {
-      id: 'show-case-mode',
-      name: intl.formatMessage({id: 'showcaseMode'}),
+      id: 'showcaseMode',
       link: '/showcase',
       icon: ShowCaseIcon
     },
     {
       id: 'language',
-      name: intl.formatMessage({id: 'language'}),
       icon: LanguageIcon
     },
-    {id: 'share', name: intl.formatMessage({id: 'share'}), icon: ShareIcon},
-    {id: 'export', name: intl.formatMessage({id: 'export'}), icon: ExportIcon},
-    {id: 'info', name: intl.formatMessage({id: 'info'}), icon: InfoIcon}
+    {id: 'share', icon: ShareIcon},
+    {id: 'export', icon: ExportIcon},
+    {id: 'info', icon: InfoIcon}
   ];
-
-  const [isOpen, setIsOpen] = useState(false);
-  const onButtonClickHandler = () => setIsOpen(!isOpen);
 
   return (
     <div className={styles.menuContainer}>
-      <button
-        onClick={() => onButtonClickHandler()}
-        title={intl.formatMessage({id: 'more'})}
-        className={styles.menuButton}>
-        <FormattedMessage id="more" />
-      </button>
-      {isOpen && (
-        <ul className={styles.menuList} onClick={() => setIsOpen(false)}>
-          {menuItems.map(menuItem => {
-            const Icon = menuItem.icon;
-
-            return (
-              <li className={styles.menuListItem} key={menuItem.id}>
-                {menuItem.link ? (
-                  <Link to={menuItem.link}>
-                    <Icon /> {menuItem.name}
-                  </Link>
-                ) : (
-                  <React.Fragment>
-                    <Icon /> {menuItem.name}
-                  </React.Fragment>
-                )}
-
-                {menuItem.id === 'language' && <LanguageSelector />}
-              </li>
-            );
-          })}
-        </ul>
-      )}
+      <ul className={styles.menuList}>
+        {menuItems.map(menuItem => (
+          <li className={styles.menuListItem} key={menuItem.id}>
+            {menuItem.link ? (
+              <Button
+                label={menuItem.id}
+                link={menuItem.link}
+                icon={menuItem.icon}
+              />
+            ) : (
+              <Button
+                label={menuItem.id}
+                icon={menuItem.icon}
+                onClick={() => console.log('placeholder')}
+              />
+            )}
+            {menuItem.id === 'language' && <LanguageSelector />}
+          </li>
+        ))}
+      </ul>
+      <div className={styles.logo}>
+        <CCILogo />
+      </div>
     </div>
   );
 };
