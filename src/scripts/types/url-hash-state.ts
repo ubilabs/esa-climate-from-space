c7a26045f4d9777b18347dded162ac123b2da42e
Katherina Marcenko <45818654+KatvonRivia@users.noreply.github.com>
2020-03-10 12:34:55 +0100

    refactor(selected-layers): add selected layer ids to url (#282)
    * refactor(selected-layers): add selected layer ids to url
    
    * refactor(selected-layers): add empty line
    
    * refactor(url): change separator char
    
    * refactor(selected-layers): remove console log

---
+++ src/scripts/types/url-hash-state.ts
@@ -0,0 +1,9 @@
+import {GlobeState} from '../reducers/globe/index';
+
+export interface UrlHashState {
+  globeState: GlobeState;
+  layerIds: {
+    mainId: string | null;
+    compareId: string | null;
+  };
+}
