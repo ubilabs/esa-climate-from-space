7019a194ea57c703c8b3fe9aacb28fda68b372f8
KatvonRivia <katvonrivia@gmail.com>
2020-03-18 17:44:13 +0100

    refactor(story-content): add video component to stories

--- src/scripts/components/story/story.tsx
+++ src/scripts/components/story/story.tsx
@@ -9,8 +9,10 @@ import StoryMedia from '../story-media/story-media';
 import StoryFooter from '../story-footer/story-footer';
 import fetchStory from '../../actions/fetch-story';
 import Header from '../header/header';
+import StoryVideo from '../story-video/story-video';
 
 import {StoryMode} from '../../types/story-mode';
+import {Slide, Story as StoryType} from '../../types/story';
 
 import styles from './story.styl';
 
@@ -40,6 +42,16 @@ const Story: FunctionComponent = () => {
     return null;
   }
 
+  const getRightSideComponent = (slide: Slide, story: StoryType) => {
+    if (slide.images) {
+      return <StoryMedia images={slide.images} storyId={story.id} />;
+    } else if (slide.videoId) {
+      return <StoryVideo videoId={slide.videoId} />;
+    }
+
+    return <Globes />;
+  };
+
   return (
     <div className={storyClasses}>
       {storyListItem && (
@@ -61,14 +73,7 @@ const Story: FunctionComponent = () => {
                   storyId={selectedStory.id}
                   slide={currentSlide}
                 />
-                {currentSlide.images ? (
-                  <StoryMedia
-                    images={currentSlide.images}
-                    storyId={selectedStory.id}
-                  />
-                ) : (
-                  <Globes />
-                )}
+                {getRightSideComponent(currentSlide, selectedStory)}
               </React.Fragment>
             )
         )}
