a2f5a02a9122be57244a05e0137da1b134072e6d
Philipp Wambach <pwambach@users.noreply.github.com>
2020-03-18 16:35:30 +0100

    feat(electron): add offline stories (#299)

---
+++ src/scripts/libs/electron/get-offline-story-media-url.ts
@@ -0,0 +1,9 @@
+// Returns the url template for offline usage
+export function getOfflineStoryMediaUrl(): string {
+  if (!window.cfs) {
+    console.error('Calling electron function from a non-electron environment');
+    return '';
+  }
+
+  return window.cfs.getDownloadsPath('downloads', 'story-{id}');
+}
