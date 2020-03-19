c52dc7f8edd04d47ad50853838da0777d34a547e
Philipp Wambach <philippwambach@googlemail.com>
2020-03-19 17:47:01 +0100

    feat(download-button): add progress

--- src/scripts/components/download-button/download-button.tsx
+++ src/scripts/components/download-button/download-button.tsx
@@ -4,10 +4,15 @@ import {useSelector} from 'react-redux';
 import {isElectron, downloadUrl, deleteId} from '../../libs/electron/index';
 import {downloadedDataSelector} from '../../selectors/offline/downloaded';
 import {downloadProgressSelector} from '../../selectors/offline/progress';
+import {DownloadIcon} from '../icons/download-icon';
+import {DownloadCompleteIcon} from '../icons/download-complete-icon';
+import Button from '../button/button';
+
+import styles from './download-button.styl';
 
 interface Props {
-  url: string;
   id: string;
+  url: string;
 }
 
 export const DownloadButton: FunctionComponent<Props> = ({url, id}) => {
@@ -30,19 +35,34 @@ export const DownloadButton: FunctionComponent<Props> = ({url, id}) => {
   }
 
   return (
-    <div>
-      {!isDownloaded && <button onClick={onDownload}>Download</button>}
+    <div className={styles.downloadButton}>
+      {!isDownloaded && !isDownloading && (
+        <Button
+          icon={DownloadIcon}
+          onClick={onDownload}
+          className={styles.notDownloaded}
+        />
+      )}
       {isDownloaded && (
-        <button
+        <Button
+          className={styles.complete}
+          icon={DownloadCompleteIcon}
           onClick={event => {
             event.stopPropagation();
             event.preventDefault();
             deleteId(id);
-          }}>
-          Delete
-        </button>
+          }}
+        />
       )}
-      {isDownloading && <span>{progress}</span>}
+      {isDownloading && (
+        <svg
+          className={styles.circle}
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 40 40"
+          style={{strokeDashoffset: 100 - progress}}>
+          <circle cx="20" cy="20" r="16" />
+        </svg>
+      )}
     </div>
   );
 };
---
+++ src/scripts/components/icons/download-complete-icon.tsx
@@ -0,0 +1,15 @@
+import React, {FunctionComponent} from 'react';
+
+export const DownloadCompleteIcon: FunctionComponent = () => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 18 18"
+    xmlns="http://www.w3.org/2000/svg">
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M3.75 8.025L7.2 11.475L14.25 4.5L12.75 3L7.2 8.55L5.25 6.6L3.75 8.025ZM14.25 13.5H3.75V15H14.25V13.5Z"
+    />
+  </svg>
+);
