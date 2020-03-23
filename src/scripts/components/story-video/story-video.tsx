5d8bd27b5d30c68a49df0b6a9d5f700b55c11bd5
Katherina Marcenko <45818654+KatvonRivia@users.noreply.github.com>
2020-03-23 17:19:51 +0100

    feat(video): add test video with subtitles (#305)

--- src/scripts/components/story-video/story-video.tsx
+++ src/scripts/components/story-video/story-video.tsx
@@ -11,7 +11,7 @@ const StoryVideo: FunctionComponent<Props> = ({videoId}) => (
     <iframe
       width="100%"
       height="100%"
-      src={`https://www.youtube.com/embed/${videoId}?rel=0`}
+      src={`https://www.youtube.com/embed/${videoId}?rel=0&cc_load_policy=1`}
       frameBorder="0"
       allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen></iframe>
