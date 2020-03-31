f3800eac9591978d26e97c06cb0a70fd9fc47535
KatvonRivia <katvonrivia@gmail.com>
2020-03-31 15:10:45 +0200

    feat(video): set subtitle language

--- src/scripts/components/story-video/story-video.tsx
+++ src/scripts/components/story-video/story-video.tsx
@@ -1,4 +1,7 @@
 import React, {FunctionComponent} from 'react';
+import {useSelector} from 'react-redux';
+
+import {languageSelector} from '../../selectors/language';
 
 import styles from './story-video.styl';
 
@@ -6,16 +9,20 @@ interface Props {
   videoId: string;
 }
 
-const StoryVideo: FunctionComponent<Props> = ({videoId}) => (
-  <div className={styles.storyVideo}>
-    <iframe
-      width="100%"
-      height="100%"
-      src={`https://www.youtube.com/embed/${videoId}?rel=0&cc_load_policy=1`}
-      frameBorder="0"
-      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen></iframe>
-  </div>
-);
+const StoryVideo: FunctionComponent<Props> = ({videoId}) => {
+  const language = useSelector(languageSelector);
+
+  return (
+    <div className={styles.storyVideo}>
+      <iframe
+        width="100%"
+        height="100%"
+        src={`https://www.youtube.com/embed/${videoId}?rel=0&cc_load_policy=1&cc_lang_pref=${language}`}
+        frameBorder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen></iframe>
+    </div>
+  );
+};
 
 export default StoryVideo;
