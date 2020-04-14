92f9602356a3c6cbd5f1f583d9a2b732020b2ed3
Katherina Marcenko <45818654+KatvonRivia@users.noreply.github.com>
2020-04-14 16:30:57 +0200

    feat(story-footer): slide in story footer on mouse move (#327)
    * feat(story-footer): fade in story footer on hover
    
    * feat(story-footer): hide footer when mouse stops moving
    
    * feat(story-footer): clean up event listener
    
    * style(story-footer): add empty line before return

--- src/scripts/components/story-footer/story-footer.tsx
+++ src/scripts/components/story-footer/story-footer.tsx
@@ -3,11 +3,13 @@ import React, {FunctionComponent} from 'react';
 import StoryPagination from '../story-pagination/story-pagination';
 import Autoplay from '../autoplay/autoplay';
 import {useStoryNavigation} from '../../hooks/use-story-navigation';
+import cx from 'classnames';
 
 import {StoryMode} from '../../types/story-mode';
 import {Story} from '../../types/story';
 
 import styles from './story-footer.styl';
+import {useMouseMove} from '../../hooks/use-mouse-move';
 
 interface Props {
   mode: StoryMode | null;
@@ -21,10 +23,17 @@ const StoryFooter: FunctionComponent<Props> = ({
   selectedStory
 }) => {
   const isShowcaseMode = mode === StoryMode.Showcase;
+  const isStoriesMode = mode === StoryMode.Stories;
   const {nextSlideLink, previousSlideLink, autoPlayLink} = useStoryNavigation();
+  const mouseMove = useMouseMove();
+
+  const footerClasses = cx(
+    styles.storyFooter,
+    !isStoriesMode && !mouseMove && styles.slideOutFooter
+  );
 
   return (
-    <div className={styles.storyFooter}>
+    <div className={footerClasses}>
       {selectedStory && (
         <StoryPagination
           mode={mode}
