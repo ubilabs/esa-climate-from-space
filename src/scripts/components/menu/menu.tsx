cc6e8198e17fe9306fcd3b54419a88ffbac5c70d
KatvonRivia <katvonrivia@gmail.com>
2020-04-15 15:11:19 +0200

    style(menu): remove block statement

--- src/scripts/components/menu/menu.tsx
+++ src/scripts/components/menu/menu.tsx
@@ -10,61 +10,59 @@ import {DownloadIcon} from '../icons/download-icon';
 
 import styles from './menu.styl';
 
-const Menu: FunctionComponent = () => {
-  return (
-    <nav className={styles.menuContainer}>
-      <ul className={styles.menuList}>
-        <li className={styles.menuItemTitle}>
-          <PresenterIcon /> <FormattedMessage id={'modes'} />
-        </li>
-        <li className={styles.menuListItem}>
-          <Button
-            className={styles.menuButton}
-            label={'presenterMode'}
-            link={'/present'}
-          />
-        </li>
-        <li className={styles.menuListItem}>
-          <Button
-            className={styles.menuButton}
-            label={'showcaseMode'}
-            link={'/showcase'}
-          />
-        </li>
-        <li className={styles.subMenuTitle}>
-          <DownloadIcon /> <FormattedMessage id={'offline'} />
-        </li>
-        <li className={styles.menuListItem}>
-          <Button className={styles.menuButton} label={'download'} />
-        </li>
-      </ul>
-      <ul className={styles.menuList}>
-        <li className={styles.menuItemTitle}>
-          <LanguageIcon /> <FormattedMessage id={'language'} />
-        </li>
-        <li>
-          <LanguageSelector className={styles.menuListItem} />
-        </li>
-      </ul>
-      <ul className={styles.menuList}>
-        <li className={styles.menuItemTitle}>
-          <InfoIcon /> <FormattedMessage id={'info'} />
-        </li>
-        <li className={styles.menuListItem}>
-          <Button className={styles.menuButton} label={'about'} />
-        </li>
-        <li className={styles.menuListItem}>
-          <Button className={styles.menuButton} label={'EsaWebsite'} />
-        </li>
-        <li className={styles.menuListItem}>
-          <Button className={styles.menuButton} label={'CCIWebsite'} />
-        </li>
-        <li className={styles.menuListItem}>
-          <Button className={styles.menuButton} label={'github'} />
-        </li>
-      </ul>
-    </nav>
-  );
-};
+const Menu: FunctionComponent = () => (
+  <nav className={styles.menuContainer}>
+    <ul className={styles.menuList}>
+      <li className={styles.menuItemTitle}>
+        <PresenterIcon /> <FormattedMessage id={'modes'} />
+      </li>
+      <li className={styles.menuListItem}>
+        <Button
+          className={styles.menuButton}
+          label={'presenterMode'}
+          link={'/present'}
+        />
+      </li>
+      <li className={styles.menuListItem}>
+        <Button
+          className={styles.menuButton}
+          label={'showcaseMode'}
+          link={'/showcase'}
+        />
+      </li>
+      <li className={styles.subMenuTitle}>
+        <DownloadIcon /> <FormattedMessage id={'offline'} />
+      </li>
+      <li className={styles.menuListItem}>
+        <Button className={styles.menuButton} label={'download'} />
+      </li>
+    </ul>
+    <ul className={styles.menuList}>
+      <li className={styles.menuItemTitle}>
+        <LanguageIcon /> <FormattedMessage id={'language'} />
+      </li>
+      <li>
+        <LanguageSelector className={styles.menuListItem} />
+      </li>
+    </ul>
+    <ul className={styles.menuList}>
+      <li className={styles.menuItemTitle}>
+        <InfoIcon /> <FormattedMessage id={'info'} />
+      </li>
+      <li className={styles.menuListItem}>
+        <Button className={styles.menuButton} label={'about'} />
+      </li>
+      <li className={styles.menuListItem}>
+        <Button className={styles.menuButton} label={'EsaWebsite'} />
+      </li>
+      <li className={styles.menuListItem}>
+        <Button className={styles.menuButton} label={'CCIWebsite'} />
+      </li>
+      <li className={styles.menuListItem}>
+        <Button className={styles.menuButton} label={'github'} />
+      </li>
+    </ul>
+  </nav>
+);
 
 export default Menu;
