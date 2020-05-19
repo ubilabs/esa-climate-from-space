17d0c801825ed71274fda8c14eb2712266d4089a
Philipp Wambach <philippwambach@googlemail.com>
2020-05-19 17:02:10 +0200

    feat(story-tags): add story filter bar

--- src/scripts/components/icons/arrow-left-icon.tsx
+++ src/scripts/components/icons/arrow-left-icon.tsx
@@ -1,15 +1,15 @@
 import React, {FunctionComponent} from 'react';
 
-export const DownloadIcon: FunctionComponent = () => (
+export const ArrowLeftIcon: FunctionComponent = () => (
   <svg
     width="24"
     height="24"
     viewBox="0 0 24 24"
+    fill="none"
     xmlns="http://www.w3.org/2000/svg">
     <path
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M15 9H19L12 16L5 9H9V3H15V9ZM5 20V18H19V20H5Z"
+      d="M15.41 16.59L10.83 12L15.41 7.41L14 6L8 12L14 18L15.41 16.59Z"
+      fill="currentColor"
     />
   </svg>
 );
--- src/scripts/components/url-sync/url-sync.tsx
+++ src/scripts/components/url-sync/url-sync.tsx
@@ -3,7 +3,9 @@ import {useHistory, useLocation} from 'react-router-dom';
 import {useSelector} from 'react-redux';
 
 import {globeStateSelector} from '../../selectors/globe/globe-state';
-import {getParamString} from '../../libs/globe-url-parameter';
+import {selectedTagsSelector} from '../../selectors/story/selected-tags';
+import {getParamString as getGlobeParamString} from '../../libs/globe-url-parameter';
+import {getParamString as getTagsParamString} from '../../libs/tags-url-parameter';
 import {selectedLayerIdsSelector} from '../../selectors/layers/selected-ids';
 
 // syncs the query parameters of the url when values change in store
@@ -11,11 +13,12 @@ const UrlSync: FunctionComponent = () => {
   const history = useHistory();
   const location = useLocation();
   const globeState = useSelector(globeStateSelector);
+  const selectedTags = useSelector(selectedTagsSelector);
   const {mainId, compareId} = useSelector(selectedLayerIdsSelector);
 
   // set globe query params in url when globe state changes
   useEffect(() => {
-    const globeValue = getParamString(globeState, mainId, compareId);
+    const globeValue = getGlobeParamString(globeState, mainId, compareId);
 
     if (!globeValue) {
       return;
@@ -27,6 +30,22 @@ const UrlSync: FunctionComponent = () => {
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [globeState, mainId, compareId, history]); // we don't want to check for location.search changes
+
+  // set story tag query params in url when tags state changes
+  useEffect(() => {
+    const tagsValue = getTagsParamString(selectedTags);
+    const params = new URLSearchParams(location.search);
+
+    if (!tagsValue) {
+      params.delete('tags');
+    } else {
+      params.set('tags', tagsValue);
+    }
+
+    history.replace({search: params.toString()});
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selectedTags, history]); // we don't want to check for location.search changes
 
   return null;
 };
