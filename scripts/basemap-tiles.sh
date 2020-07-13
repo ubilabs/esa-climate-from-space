b592f80ad2968dcd10c65bc0b568e5915a04e37e
Philipp Wambach <philippwambach@googlemail.com>
2020-07-13 15:19:05 +0200

    fix(scripts): fix docs

--- scripts/basemap-tiles.sh
+++ scripts/basemap-tiles.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# Usage: ./basemap-tiles.sh <input-file> <output-folder>
-# with Docker: docker run -v `pwd`:/data -w /data --name gdal2 -it --rm geographica/gdal2 ./basemap-tiles <input-file> <output-folder>
+# Usage: ./scripts/basemap-tiles.sh <input-file> <output-folder>
+# with Docker: docker run -v `pwd`:/data -w /data --name gdal2 -it --rm geographica/gdal2 ./scripts/basemap-tiles.sh <input-file> <output-folder>
 
 # Images and worldfiles can be found here: https://drive.google.com/drive/folders/15UR1BBZRVPvLlvPF3s5yYNqpdmd6PMcF
 # Upload tiles with: gsutil -m cp -r ./<output-folder>/ gs://esa-cfs-tiles/<version>/basemaps/<name>
