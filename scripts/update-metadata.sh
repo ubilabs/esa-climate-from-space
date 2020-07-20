8cdaac48b89384ba645b940517a737fe3b74e429
Patrick Mast <mast@ubilabs.net>
2020-07-20 17:42:45 +0200

    feat(update metadata): use merge instead of copying piece by piece

--- scripts/update-metadata.sh
+++ scripts/update-metadata.sh
@@ -7,8 +7,6 @@
 version=0.6.1
 workingDir="./download"
 layersConfigFile="../data/layers-config.json"
-layersConfig=$(cat $layersConfigFile)
-#keys=$(jq -r 'keys[]' <<< "$layersConfig" | head -n 1)
 
 # take optional layer ID from command line argument
 layerID=$1 
@@ -16,7 +14,7 @@ layerID=$1
 # check if layer ID is set
 if [ -z $layerID ]
 then
-    keys=$(jq -r 'keys[]' <<< "$layersConfig")
+    keys=$(jq -r 'keys[]' $layersConfigFile)
 else
     keys=$layerID
 fi
@@ -28,30 +26,16 @@ do
 
     # download metadata file
     path=gs://esa-cfs-tiles/$version/$datasetId/metadata.json
-    metadataFileName=$datasetId-metadata.json
-    gsutil cp $path $workingDir/$metadataFileName
+    metadataFileName=$workingDir/$datasetId-metadata.json
+    gsutil -q cp $path $metadataFileName
 
-    # iterate over config for dataset
-    props=$(jq -r ".\"$datasetId\" | keys[]"  <<< "$layersConfig")
-    for prop in ${props}
-    do
-        # extract new vlaue from layer config
-        value=$(jq ".\"$datasetId\".\"$prop\""  <<< "$layersConfig")
-        
-        replacement=".\"$prop\" = $value"
+    # merge layer config into metdata file
+    jq -s ".[0] * .[1].\"$datasetId\"" $metadataFileName $layersConfigFile | gsutil -q cp - $path
 
-        # overwrite property in corresponding metadata file
-        # upload metedata file
-        jq "$replacement" $workingDir/$metadataFileName > $workingDir/tmp-$metadataFileName
-
-        # replace original file with output, overwriting oriignal file does not seem to work
-        mv $workingDir/tmp-$metadataFileName $workingDir/$metadataFileName
-    done
-    # set cache header
-    gsutil -q cp $workingDir/$metadataFileName $path
+    # set cachhing headers foir remote file
     gsutil -q setmeta -r -h "cache-control: no-cache" $path
 
     # delete files
-    rm $workingDir/$metadataFileName
+    rm $metadataFileName
 done
-rm $workingDir
+rm -r $workingDir
