e33226fa18140168df6eb59b1b2aefdda6a0b066
KatvonRivia <katvonrivia@gmail.com>
2020-07-27 18:15:38 +0200

    refactor(splashscreen): adjust line height

--- src/scripts/components/splash-screen/splash-screen.tsx
+++ src/scripts/components/splash-screen/splash-screen.tsx
@@ -21,7 +21,7 @@ const SplashScreen: FunctionComponent<Props> = ({storyId, slide}) => {
       style={{
         background: `center / cover no-repeat url(${imageUrl}) rgba(0, 0, 0, 0.3)`
       }}>
-      <div className={styles.title}>
+      <div className={styles.content}>
         <ReactMarkdown
           source={slide.text}
           allowedTypes={[
