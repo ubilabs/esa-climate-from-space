9a012a38aa8200f33d2ef3de2952ecad031e97d7
KatvonRivia <katvonrivia@gmail.com>
2020-07-29 12:44:36 +0200

    refactor(layerFlyTo): remove flyTo from layerListItem

--- src/scripts/types/layer-list.d.ts
+++ src/scripts/types/layer-list.d.ts
@@ -1,12 +1,9 @@
-import {GlobeView} from './globe-view';
-
 interface LayerListItem {
   id: string;
   name: string;
   description: string;
   link: string;
   subLayers: LayerListItem[];
-  flyTo: GlobeView;
 }
 
 export type LayerList = LayerListItem[];
