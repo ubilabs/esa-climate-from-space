838194fbe11fd27dc42557a8fd41955204e4bc97
KatvonRivia <katvonrivia@gmail.com>
2020-08-17 16:35:55 +0200

    refactor(story-selector): hide additional story tags, fix selector styles

--- src/scripts/components/stories/story-tags/story-tags.tsx
+++ src/scripts/components/stories/story-tags/story-tags.tsx
@@ -1,4 +1,4 @@
-import React, {FunctionComponent} from 'react';
+import React, {FunctionComponent, useState} from 'react';
 import {FormattedMessage} from 'react-intl';
 import {useDispatch} from 'react-redux';
 import cx from 'classnames';
@@ -14,30 +14,57 @@ interface Props {
 
 const StoryTags: FunctionComponent<Props> = ({tags, selected}) => {
   const dispatch = useDispatch();
+  const [showAllTags, setShowAllTags] = useState(false);
   const toggleTag = (tag: string) => {
     const newTags = selected.includes(tag)
       ? selected.filter(oldTag => oldTag !== tag)
       : selected.concat([tag]);
-
     dispatch(setSelectedStoryTags(newTags));
   };
   const getTagClasses = (tag: string) =>
     cx(styles.tag, selected.includes(tag) && styles.selected);
 
+  const sortedTags = tags.sort((a, b) => {
+    const isSelectedA = selected.includes(a);
+    const isSelectedB = selected.includes(b);
+    if (isSelectedA && !isSelectedB) {
+      return -1;
+    }
+    if (isSelectedB && !isSelectedA) {
+      return 1;
+    }
+    return 0;
+  });
+
+  const tagCount = 3;
+
   return (
     <div className={styles.tags}>
-      {tags.map(tag => (
+      {sortedTags
+        .slice(0, showAllTags ? sortedTags.length : tagCount)
+        .map(tag => (
+          <span
+            className={getTagClasses(tag)}
+            key={tag}
+            onClick={(event: React.MouseEvent<HTMLSpanElement>) => {
+              event.preventDefault();
+              event.stopPropagation();
+              toggleTag(tag);
+            }}>
+            <FormattedMessage id={`tags.${tag}`} />
+          </span>
+        ))}
+      {!showAllTags && sortedTags.length > tagCount && (
         <span
-          className={getTagClasses(tag)}
-          key={tag}
+          className={styles.tag}
           onClick={(event: React.MouseEvent<HTMLSpanElement>) => {
             event.preventDefault();
             event.stopPropagation();
-            toggleTag(tag);
+            setShowAllTags(true);
           }}>
-          <FormattedMessage id={`tags.${tag}`} />
+          +{sortedTags.length - tagCount}
         </span>
-      ))}
+      )}
     </div>
   );
 };
