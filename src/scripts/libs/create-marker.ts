751a5acd4d4f7ae764b6bd42235f90155af8ce8f
KatvonRivia <katvonrivia@gmail.com>
2020-08-21 15:26:41 +0200

    refactor(story-markers): change marker style

--- src/scripts/libs/create-marker.ts
+++ src/scripts/libs/create-marker.ts
@@ -3,9 +3,7 @@ import {
   BillboardGraphics,
   Entity,
   ConstantProperty,
-  VerticalOrigin,
-  HorizontalOrigin,
-  Cartesian2
+  VerticalOrigin
 } from 'cesium';
 
 import NotesEsaBold from '../../../assets/fonts/NotesEsaBol.otf';
@@ -39,8 +37,7 @@ export async function createMarker(marker: Marker): Promise<Entity> {
           billboard: new BillboardGraphics({
             image: new ConstantProperty(canvas),
             verticalOrigin: new ConstantProperty(VerticalOrigin.TOP),
-            horizontalOrigin: new ConstantProperty(HorizontalOrigin.LEFT),
-            pixelOffset: new ConstantProperty(new Cartesian2(0, 0))
+            horizontalOrigin: new ConstantProperty(HorizontalOrigin.LEFT)
           })
         })
       );
@@ -83,97 +80,36 @@ async function getSvgString(storyTitle: string) {
     <div
       xmlns="http://www.w3.org/1999/xhtml">
       <div style="position: relative; display: flex; flex-direction: row;">
-        <svg
-          width="38"
-          height="49"
-          viewBox="0 0 38 49"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <g filter="url(#filter0_ddd)">
-            <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-              d="M5.19895 25.8993C3.80148 23.5432 3 20.7977 3 17.8666C3 9.10372 10.1634 2 19 2C27.8366 2 35 9.10372 35 17.8666C35 20.7977 34.1985 23.5432 32.801 25.8993H32.8096C32.0437 27.0863 31.0706 29.0672 25.3139 33.7809C19.5571 38.4946 21.1684 45 18.9943 45H19.0057C16.8316 45 18.4429 38.4946 12.6861 33.7809C6.92938 29.0672 5.95634 27.0863 5.19041 25.8993H5.19895Z"
-              fill="#00AE9D"
-            />
-          </g>
-          <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M19 24C22.3137 24 25 21.3137 25 18C25 14.6863 22.3137 12 19 12C15.6863 12 13 14.6863 13 18C13 21.3137 15.6863 24 19 24Z"
-            fill="white"
-          />
-          <defs>
-            <filter
-              id="filter0_ddd"
-              x="0"
-              y="0"
-              width="38"
-              height="49"
-              filterUnits="userSpaceOnUse"
-              color-interpolation-filters="sRGB"
-            >
-              <feFlood flood-opacity="0" result="BackgroundImageFix" />
-              <feColorMatrix
-                in="SourceAlpha"
-                type="matrix"
-                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-              />
-              <feOffset dy="1" />
-              <feGaussianBlur stdDeviation="1.5" />
-              <feColorMatrix
-                type="matrix"
-                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.2 0"
-              />
-              <feBlend
-                mode="normal"
-                in2="BackgroundImageFix"
-                result="effect1_dropShadow"
-              />
-              <feColorMatrix
-                in="SourceAlpha"
-                type="matrix"
-                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-              />
-              <feOffset dy="2" />
-              <feGaussianBlur stdDeviation="1" />
-              <feColorMatrix
-                type="matrix"
-                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.12 0"
-              />
-              <feBlend
-                mode="normal"
-                in2="effect1_dropShadow"
-                result="effect2_dropShadow"
-              />
-              <feColorMatrix
-                in="SourceAlpha"
-                type="matrix"
-                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-              />
-              <feOffset />
-              <feGaussianBlur stdDeviation="1" />
-              <feColorMatrix
-                type="matrix"
-                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.14 0"
-              />
-              <feBlend
-                mode="normal"
-                in2="effect2_dropShadow"
-                result="effect3_dropShadow"
-              />
-              <feBlend
-                mode="normal"
-                in="SourceGraphic"
-                in2="effect3_dropShadow"
-                result="shape"
-              />
-            </filter>
-          </defs>
-        </svg>
+      <svg width="32" height="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g filter="url(#filter0_ddd)">
+      <circle cx="12" cy="11" r="9" fill="#00AE9D"/>
+      </g>
+      <circle cx="12" cy="11" r="3.75" fill="white"/>
+      <defs>
+      <filter id="filter0_ddd" x="0" y="0" width="24" height="24" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+      <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+      <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+      <feOffset dy="1"/>
+      <feGaussianBlur stdDeviation="1.5"/>
+      <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.2 0"/>
+      <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow"/>
+      <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+      <feOffset dy="2"/>
+      <feGaussianBlur stdDeviation="1"/>
+      <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.12 0"/>
+      <feBlend mode="normal" in2="effect1_dropShadow" result="effect2_dropShadow"/>
+      <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+      <feOffset/>
+      <feGaussianBlur stdDeviation="1"/>
+      <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.14 0"/>
+      <feBlend mode="normal" in2="effect2_dropShadow" result="effect3_dropShadow"/>
+      <feBlend mode="normal" in="SourceGraphic" in2="effect3_dropShadow" result="shape"/>
+      </filter>
+      </defs>
+      </svg>
 
-        <div style="display: flex; margin-top: 7px;">
+
+        <div style="display: flex; margin-top: 3px;">
             <svg
             style="z-index: 1"
             height="49"
