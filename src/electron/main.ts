b0fb48032c820bb18158036dd55b794aa1194151
Malte Modrow <modrow@ubilabs.net>
2020-08-25 16:43:22 +0200

    fix(electron): handle deprecation warning of "remote" module

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -17,7 +17,8 @@ function createWindow() {
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
+      enableRemoteModule: true
     }
   });
 
