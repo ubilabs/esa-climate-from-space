62faac23e56686bb163ada75b7c92f3033308b73
Philipp Wambach <pwambach@users.noreply.github.com>
2020-08-27 16:42:30 +0200

    feat(data-layers): add first lake version and minor updates (#537)

---
+++ data/downloads/odp-ftp-lakes-chla_mean.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+BASE_URL="http://dap.ceda.ac.uk/neodc/esacci/lakes/data/lake_products/L3S/v1.0/"
+START_DATE=1992-10-01
+OUTPUT_FOLDER=./download/lakes-chla_mean
+
+mkdir -p $OUTPUT_FOLDER
+
+for i in {0..23}
+do
+  NEXT_YEAR=$(date +%Y -d "$START_DATE + $i month")
+  NEXT_DATE=$(date +%Y%m%d -d "$START_DATE + $i month")
+  FILENAME=$OUTPUT_FOLDER/$NEXT_DATE.nc
+  FTP_URL=$BASE_URL/$NEXT_YEAR/ESACCI-LAKES-L3S-LK_PRODUCTS-MERGED-$NEXT_DATE-fv1.0.nc
+  echo $FTP_URL
+
+  curl --silent $FTP_URL > $FILENAME
+
+  python ./data/drop-unused-vars.py --file $FILENAME --variable chla_mean
+done
