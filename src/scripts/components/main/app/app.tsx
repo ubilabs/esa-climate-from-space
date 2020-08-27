429b4ba99bbcaf08f4527345510cac815ff92b69
Katherina Marcenko <45818654+KatvonRivia@users.noreply.github.com>
2020-08-27 09:27:04 +0200

    refactor(story-markers): use markers on story globe (#515)
    * refactor(story-markers): change marker style
    
    * refactor(story-markers): change marker style
    
    * refactor(story-makers): change marker positions
    
    * refactor(story-markers): use markers on story globe
    
    * fix(packages): remove updates
    
    * refactor(marker): make id optional (#518)
    
    * refactor(story-markers): add markerLink as entity property

--- src/scripts/components/main/app/app.tsx
+++ src/scripts/components/main/app/app.tsx
@@ -22,9 +22,9 @@ import ShowcaseSelector from '../../stories/showcase-selector/showcase-selector'
 import Globes from '../globes/globes';
 
 import translations from '../../../i18n';
+import {useStoryMarkers} from '../../../hooks/use-story-markers';
 
 import styles from './app.styl';
-import {useStoryMarkers} from '../../../hooks/use-story-markers';
 
 // create redux store
 const store = createReduxStore();
