e04f5c90e3856c0638061211e80ee61c2bf9813a
KatvonRivia <katvonrivia@gmail.com>
2020-08-28 09:22:42 +0200

    feat(layer-icons): add layer icons to selected layers

--- src/scripts/components/layers/selected-layer-list-item/selected-layer-list-item.tsx
+++ src/scripts/components/layers/selected-layer-list-item/selected-layer-list-item.tsx
@@ -1,6 +1,8 @@
 import React, {FunctionComponent} from 'react';
 
 import {RemoveIcon} from '../../main/icons/remove-icon';
+import {replaceUrlPlaceholders} from '../../../libs/replace-url-placeholders';
+import config from '../../../config/main';
 
 import {LayerListItem} from '../../../types/layer-list';
 
@@ -11,24 +13,29 @@ interface Props {
   onRemove: () => void;
 }
 
-const SelectedLayerListItem: FunctionComponent<Props> = ({layer, onRemove}) => (
-  <div className={styles.selectedLayerListItem}>
-    <div className={styles.layerTitle}>
-      <div className={styles.layerIcon}></div>
-      <span className={styles.layerTitle}>{layer.shortName}</span>
+const SelectedLayerListItem: FunctionComponent<Props> = ({layer, onRemove}) => {
+  const layerId = layer.id;
+  const layerIconUrl = replaceUrlPlaceholders(config.api.layericon, {layerId});
+
+  return (
+    <div className={styles.selectedLayerListItem}>
+      <div className={styles.layer}>
+        <img src={layerIconUrl} className={styles.layerIcon} />
+        <span className={styles.layerTitle}>{layer.shortName}</span>
+      </div>
+      {onRemove && (
+        <button
+          className={styles.removeIcon}
+          onClick={() => {
+            if (typeof onRemove === 'function') {
+              onRemove();
+            }
+          }}>
+          <RemoveIcon />
+        </button>
+      )}
     </div>
-    {onRemove && (
-      <button
-        className={styles.removeIcon}
-        onClick={() => {
-          if (typeof onRemove === 'function') {
-            onRemove();
-          }
-        }}>
-        <RemoveIcon />
-      </button>
-    )}
-  </div>
-);
+  );
+};
 
 export default SelectedLayerListItem;
