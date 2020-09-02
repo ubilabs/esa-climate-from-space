8a89118aed782c388094f0b2d5a93fe001853128
Philipp Wambach <philippwambach@googlemail.com>
2020-09-02 11:25:19 +0200

    feat(data-layers): add split time dim script for soil moisture

---
+++ data/split-time-dim.py
@@ -0,0 +1,20 @@
+import os
+import xarray as xr
+import pandas as pd
+from datetime import datetime
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument("-f", "--file", dest="file")
+parser.add_argument("-v", "--variable", dest="variable")
+args = parser.parse_args()
+
+ds = xr.open_dataset(args.file, decode_coords=True, decode_cf=True)
+da = ds[args.variable]
+
+for n in range(len(da.time)):
+  da_slice = da.isel(time=n)
+  date = pd.to_datetime(str(da_slice.time.values))
+  filename = date.strftime('./download/soil_moisture/%Y%m%d.nc')
+  print(filename)
+  da_slice.to_dataset().to_netcdf(filename, format='NETCDF4', mode='w')
