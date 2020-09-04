e214b3a59bf28c0c0987272eee4114bf18788c14
Malte Modrow <modrow@ubilabs.net>
2020-09-04 17:20:47 +0200

    refactor(globe-action): remove not needed imports

--- src/scripts/actions/set-globe-time.ts
+++ src/scripts/actions/set-globe-time.ts
@@ -1,10 +1,3 @@
-import {Dispatch} from 'redux';
-
-import {State} from '../reducers';
-import {layerDetailsSelector} from '../selectors/layers/layer-details';
-import {getTimeRanges} from '../libs/get-time-ranges';
-import clampToRange from '../libs/clamp-to-range';
-
 export const SET_GLOBE_TIME = 'SET_GLOBE_TIME';
 
 export interface SetGlobeTimeAction {
