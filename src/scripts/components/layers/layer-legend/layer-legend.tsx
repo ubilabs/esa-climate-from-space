a7cf2361c9dcfb628354ee95a979672cc8b25417
Philipp Wambach <philippwambach@googlemail.com>
2020-09-08 15:53:56 +0200

    feat(layer-legend): use background color dependent of basemap

--- src/scripts/components/layers/layer-legend/layer-legend.tsx
+++ src/scripts/components/layers/layer-legend/layer-legend.tsx
@@ -5,12 +5,31 @@ import cx from 'classnames';
 import {replaceUrlPlaceholders} from '../../../libs/replace-url-placeholders';
 import config from '../../../config/main';
 
+import {BasemapId} from '../../../types/basemap';
+
 import styles from './layer-legend.styl';
+
+const backgroundColorMapping: {[_: string]: string} = {
+  blue: '#2A417B',
+  land: '#8E8E8E',
+  ocean: '#D4D4D4',
+  atmosphere: '#8E8E8E',
+  dark: '#262626'
+};
+
+const getBackgroundColor = (basemap: BasemapId | null) => {
+  if (!basemap) {
+    return 'transparent';
+  }
+
+  return backgroundColorMapping[basemap];
+};
 
 interface Props {
   id: string;
   values: (number | string)[];
   unit: string;
+  basemap: BasemapId | null;
   isCompare?: boolean;
 }
 
@@ -18,11 +37,13 @@ const LayerLegend: FunctionComponent<Props> = ({
   id,
   values,
   unit,
+  basemap,
   isCompare = false
 }) => (
   <div className={cx(styles.layerLegend, isCompare && styles.rightSided)}>
     <img
       className={styles.image}
+      style={{backgroundColor: getBackgroundColor(basemap)}}
       src={replaceUrlPlaceholders(config.legendImage, {
         variable: id.split('.')[1]
       })}
