065861ecb502b54cd37bdb1f750290413d443939
Philipp Wambach <philippwambach@googlemail.com>
2020-09-15 09:27:32 +0200

    feat(ozone): use new data

---
+++ data/replace-time-coordinate.py
@@ -0,0 +1,19 @@
+import os
+import xarray as xr
+import pandas as pd
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument("-f", "--file", dest="file")
+parser.add_argument("-t", "--timestamp", dest="timestamp")
+args = parser.parse_args()
+
+ds = xr.open_dataset(args.file, decode_cf=False, decode_coords=False)
+
+new_time = pd.to_datetime([args.timestamp])
+
+ds = ds.drop("time").squeeze(dim="time", drop=True)
+ds = ds.expand_dims(dim={"time": new_time}, axis=0)
+
+os.remove(args.file)
+ds.to_netcdf(args.file, format="NETCDF4_CLASSIC", mode="w")
