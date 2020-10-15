14d7543528a9f499b7c5e1d50d52753ffc716664
Katherina Marcenko <45818654+KatvonRivia@users.noreply.github.com>
2020-10-15 18:01:56 +0200

    New Version 0.10.0 (#693)
    * refactor(scrollbar): set overflow to scroll to avoid weird padding (#681)
    
    * refactor(scrollbar): set overflow to scroll to avoid weird padding
    
    * refactor(scrollbars): add scrollbar width for firefox
    
    * fix(spinning-globe): stop spinning when layer is selected or projection changes (#683)
    
    * fix(text-select): fix text select when moving globe (#685)
    
    * feat(tracking): init matomo
    
    * refactor(story-selector): hide tags on presentation modes (#687)
    
    * refactor(credits): adjust credits, move links to config (#689)
    
    * refactor(credits): adjust credits, move links to config
    
    * refactor(menu): adjust spacing
    
    * feat(tracking): implement tracking and consent
    
    * refactor(tracking): cleanup
    
    * fix(timeslider): show single slider when there is no main range (#691)
    
    * 0.10.0 (#692)
    
    Co-authored-by: Malte Modrow <modrow@ubilabs.net>

--- src/scripts/components/main/menu/menu.tsx
+++ src/scripts/components/main/menu/menu.tsx
@@ -160,6 +160,12 @@ const Menu: FunctionComponent = () => {
               <li className={styles.menuListItem}>
                 <TrackingToggle />
               </li>
+              <li className={styles.subMenuTitle}>
+                <AnalyticsIcon /> <FormattedMessage id={'analytics'} />
+              </li>
+              <li className={styles.menuListItem}>
+                <TrackingToggle />
+              </li>
             </ul>
           </React.Fragment>
         )}
