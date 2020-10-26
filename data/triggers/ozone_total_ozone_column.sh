15cfeb0960bc5f3e0c1056d11a2f8102ca0245cf
Philipp Wambach <pwambach@users.noreply.github.com>
2020-10-26 11:15:15 +0100

    fix(data-layers): update sea surface salinity description (#726)
    * feat(layers): update sss version in description
    
    * feat(data-layer): add 2019 to sss
    
    * chore(triggers): update version number
    
    * fix(layers): update sss timespan

--- data/triggers/ozone_total_ozone_column.sh
+++ data/triggers/ozone_total_ozone_column.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-TIMEOUT=8000
+TIMEOUT=12000
 LAYER_ID="ozone.total_ozone_column"
 VARIABLE_ID="total_ozone_column"
 LAYER_TYPE="image"
-VERSION="0.9.1"
+VERSION="0.10.2"
 LON_RES="720"
 LAT_RES="360"
 ZOOM_LEVELS="0-3"
@@ -20,7 +20,7 @@ if [ ! -f ./package.json ]; then
     exit 1
 fi
 
-gcloud builds submit --config ./ci/cloudbuild-tiles-reproject.yaml \
+gcloud --project esa-climate-from-space builds submit --config ./ci/cloudbuild-tiles-reproject.yaml \
   --timeout=$TIMEOUT \
   --substitutions _LAYER_ID=$LAYER_ID,_VARIABLE_ID=$VARIABLE_ID,_ZOOM_LEVELS=$ZOOM_LEVELS,_LON_RES=$LON_RES,_LAT_RES=$LAT_RES,_LAYER_TYPE=$LAYER_TYPE,_VERSION=$VERSION,_MIN=$MIN,_MAX=$MAX,_MIN_LON=$MIN_LON,_MAX_LON=$MAX_LON,_MIN_LAT=$MIN_LAT,_MAX_LAT=$MAX_LAT \
   .
