2c4beea23607d27b997893c41ce66cb76f34d83d
KatvonRivia <katvonrivia@gmail.com>
2020-12-03 12:31:02 +0100

    feat(app): hide all UI except story mode

--- src/scripts/components/main/navigation/navigation.tsx
+++ src/scripts/components/main/navigation/navigation.tsx
@@ -1,34 +1,29 @@
 import React, {FunctionComponent, useState} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
+// import {useDispatch, useSelector} from 'react-redux';
 
 import Button from '../button/button';
-import Overlay from '../overlay/overlay';
-import Menu from '../menu/menu';
-import {LayersIcon} from '../icons/layers-icon';
+// import Overlay from '../overlay/overlay';
+// import Menu from '../menu/menu';
 import {StoryIcon} from '../icons/story-icon';
-import showLayerSelectorAction from '../../../actions/show-layer-selector';
-import Share from '../share/share';
-import {MenuIcon} from '../icons/menu-icon';
-import {FilterIcon} from '../icons/filter-icon';
-import setLanguageAction from '../../../actions/set-language';
-import {languageSelector} from '../../../selectors/language';
-import LanguageBubble from '../language-bubble/language-bubble';
-import SelectedTags from '../../stories/selected-tags/selected-tags';
-import {selectedTagsSelector} from '../../../selectors/story/selected-tags';
-import config from '../../../config/main';
+// import {LayersIcon} from '../icons/layers-icon';
+// import showLayerSelectorAction from '../../../actions/show-layer-selector';
+// import Share from '../share/share';
+// import {MenuIcon} from '../icons/menu-icon';
+// import setLanguageAction from '../../../actions/set-language';
+// import LanguageBubble from '../language-bubble/language-bubble';
+// import {languageSelector} from '../../../selectors/language';
+// import config from '../../../config/main';
 
 import styles from './navigation.styl';
 
 const Navigation: FunctionComponent = () => {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
   const [showMenu, setShowMenu] = useState(false);
-  const [showTags, setShowTags] = useState(false);
-  const selectedLanguage = useSelector(languageSelector);
-  const savedLanguage = localStorage.getItem(config.localStorageLanguageKey);
-  const [showLanguageBubble, setShowLanguageBubble] = useState<boolean>(
-    !savedLanguage
-  );
-  const selectedTags = useSelector(selectedTagsSelector);
+  // const selectedLanguage = useSelector(languageSelector);
+  // const savedLanguage = localStorage.getItem(config.localStorageLanguageKey);
+  // const [showLanguageBubble, setShowLanguageBubble] = useState<boolean>(
+  //   !savedLanguage
+  // );
 
   return (
     <div className={styles.navigation}>
@@ -39,25 +34,15 @@ const Navigation: FunctionComponent = () => {
         icon={StoryIcon}
         hideLabelOnMobile
       />
-      {selectedTags.length > 0 && (
-        <React.Fragment>
-          <Button
-            className={styles.tagsButton}
-            icon={FilterIcon}
-            onClick={() => setShowTags(!showTags)}
-          />
-          <div className={styles.badge} />
-        </React.Fragment>
-      )}
-      <Button
+      {/* <Button
         className={styles.button}
         label="layers"
         onClick={() => dispatch(showLayerSelectorAction(true))}
         icon={LayersIcon}
         hideLabelOnMobile
-      />
-      <Share />
-      <Button
+      /> */}
+      {/* <Share /> */}
+      {/* <Button
         className={styles.button}
         icon={MenuIcon}
         onClick={() => setShowMenu(true)}
@@ -74,15 +59,15 @@ const Navigation: FunctionComponent = () => {
             dispatch(setLanguageAction(selectedLanguage));
           }}
         />
-      )}
-      {showMenu && (
+      )} */}
+      {/* {showMenu && (
         <Overlay onClose={() => setShowMenu(false)}>
           <Menu />
         </Overlay>
       )}
       {selectedTags.length > 0 && showTags && (
         <SelectedTags selectedTags={selectedTags} />
-      )}
+      )} */}
     </div>
   );
 };
