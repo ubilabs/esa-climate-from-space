a2ba78982f89141f7ef7f67b34732e81ae6e7ebf
Philipp Wambach <philippwambach@googlemail.com>
2020-12-14 16:50:22 +0100

    feat(dataset): update greenhouse gases

--- data/triggers/greenhouse_xch4.sh
+++ data/triggers/greenhouse_xch4.sh
@@ -20,7 +20,8 @@ if [ ! -f ./package.json ]; then
     exit 1
 fi
 
-gcloud builds submit --config ./ci/cloudbuild-tiles-reproject.yaml \
+gcloud --project esa-climate-from-space builds submit \
+  --config ./ci/cloudbuild-tiles-reproject.yaml \
   --timeout=$TIMEOUT \
   --substitutions _LAYER_ID=$LAYER_ID,_VARIABLE_ID=$VARIABLE_ID,_ZOOM_LEVELS=$ZOOM_LEVELS,_LAYER_TYPE=$LAYER_TYPE,_LON_RES=$LON_RES,_LAT_RES=$LAT_RES,_VERSION=$VERSION,_MIN=$MIN,_MAX=$MAX,_MIN_LON=$MIN_LON,_MAX_LON=$MAX_LON,_MIN_LAT=$MIN_LAT,_MAX_LAT=$MAX_LAT \
   .
