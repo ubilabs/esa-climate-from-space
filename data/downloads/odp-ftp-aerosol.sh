07038cff815161d65b458039c4459d1dbabed5bf
Philipp Wambach <philippwambach@googlemail.com>
2020-12-14 16:47:29 +0100

    feat(dataset): update aerosol to v4.3

--- data/downloads/odp-ftp-aerosol.sh
+++ data/downloads/odp-ftp-aerosol.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-BASE_URL="ftp://anon-ftp.ceda.ac.uk/neodc/esacci/aerosol/data/AATSR_SU/L3/v4.21/MONTHLY/"
-START_DATE=2002-07-01
+VERSION="4.3"
+BASE_URL="ftp://anon-ftp.ceda.ac.uk/neodc/esacci/aerosol/data/AATSR_SU/L3/v$VERSION/MONTHLY/"
+START_DATE=2012-01-01
 OUTPUT_FODLER=./download/aerosol
 
 mkdir -p $OUTPUT_FODLER
@@ -12,7 +13,7 @@ do
   NEXT_MONTH=$(date +%Y%m -d "$START_DATE + $i month")
   NEXT_DATE=$(date +%Y-%m-%d -d "$START_DATE + $i month")
   FILENAME=$OUTPUT_FODLER/$(date +%Y%m%d -d "$START_DATE + $i month").nc
-  FTP_URL=$BASE_URL$NEXT_YEAR/$NEXT_MONTH-ESACCI-L3C_AEROSOL-AER_PRODUCTS-AATSR_ENVISAT-SU_MONTHLY-v4.21.nc
+  FTP_URL=$BASE_URL$NEXT_YEAR/$NEXT_MONTH-ESACCI-L3C_AEROSOL-AER_PRODUCTS-AATSR_ENVISAT-SU_MONTHLY-v$VERSION.nc
   echo $FTP_URL
 
   curl --silent $FTP_URL > $FILENAME
