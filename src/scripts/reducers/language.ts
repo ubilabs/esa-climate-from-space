86e116af686050856e1e97765c664ca552027451
KatvonRivia <katvonrivia@gmail.com>
2021-08-03 15:26:10 +0200

    feat(url): add language to url

--- src/scripts/reducers/language.ts
+++ src/scripts/reducers/language.ts
@@ -1,11 +1,13 @@
 import {SET_LANGUAGE, SetLanguageAction} from '../actions/set-language';
 import getBrowserLanguage from '../libs/get-browser-language';
 import getLocalStorageLanguage from '../libs/get-local-storage-language';
+import { parseUrl } from '../libs/language-url-parameter';
 
 import {Language} from '../types/language';
 
-const initialState: Language =
-  getLocalStorageLanguage() || getBrowserLanguage() || Language.EN;
+
+
+const initialState: Language = parseUrl() || getLocalStorageLanguage() || getBrowserLanguage() || Language.EN;
 
 function languageReducer(
   state: Language = initialState,
