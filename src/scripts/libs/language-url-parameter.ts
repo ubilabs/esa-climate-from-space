80b089383df880d55aed876add9a370b590a2e2a
KatvonRivia <katvonrivia@gmail.com>
2021-08-03 16:52:42 +0200

    refactor(language): remove or statement

--- src/scripts/libs/language-url-parameter.ts
+++ src/scripts/libs/language-url-parameter.ts
@@ -1,6 +1,5 @@
 import {Language} from '../types/language';
 // parses window.location and reads the story tags from query params
-//
 // note: we do not use the location.search prop here because the HashRouter
 
 // stores the query parameters in the location.hash prop
@@ -11,10 +10,7 @@ export function parseUrl(): Language | null {
   const urlParams = new URLSearchParams(queryString);
   const languageParam = urlParams.get('lng') as Language;
 
-  if (
-    (languageParam && Object.values(Language).includes(languageParam)) ||
-    !languageParam
-  ) {
+  if (languageParam && Object.values(Language).includes(languageParam)) {
     return languageParam;
   }
 
