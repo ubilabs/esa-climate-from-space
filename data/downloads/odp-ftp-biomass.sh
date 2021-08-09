187fc4b2741398bf82dd45f55d5c27112c709a6b
Philipp Wambach <pwambach@users.noreply.github.com>
2021-08-09 14:22:07 +0200

    feat(dataset): update biomass dataset (#950)

--- data/downloads/odp-ftp-biomass.sh
+++ data/downloads/odp-ftp-biomass.sh
@@ -8,13 +8,12 @@ OUTPUT_FODLER=./download
 for YEAR in $YEARS
 do
     FTP_URL="ftp://anon-ftp.ceda.ac.uk/neodc/esacci/biomass/data/agb/maps/$DATA_VERSION/netcdf/$YEAR/ESACCI-BIOMASS-L4-AGB-MERGED-100m-$YEAR-f$DATA_VERSION.nc"
-    FILENAME="$OUTPUT_FODLER/$YEAR0101".nc
+    FILENAME="$OUTPUT_FODLER/$YEAR".nc
 
     echo $FTP_URL
 
     curl --silent $FTP_URL > $FILENAME
 
-    # TODO
-    # python ./data/drop-unused-vars.py --file $FILENAME --variable AOD550_mean
-    # python ./data/add-time-coordinate.py --file $FILENAME --timestamp $(date -d "$YEAR-01-01")
+    # TODO - for now timestamps are overwritten by 'layers-config.json' enrty
+    # python ./data/add-time-coordinate.py --file $FILENAME --timestamp $(date +%Y-%m-%d -d "$YEAR-01-01")
 done
