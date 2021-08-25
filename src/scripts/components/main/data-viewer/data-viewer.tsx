1a5b3c8540e6faadf1a173911d3349e396c96979
Philipp Wambach <philippwambach@googlemail.com>
2021-08-25 15:30:40 +0200

    feat(legend): add ability to hide legend

--- src/scripts/components/main/data-viewer/data-viewer.tsx
+++ src/scripts/components/main/data-viewer/data-viewer.tsx
@@ -148,25 +148,34 @@ const DataViewer: FunctionComponent<Props> = ({
     <div className={styles.dataViewer}>
       {[mainLayerDetails, compareLayerDetails]
         .filter((layer): layer is Layer => Boolean(layer))
-        .map(({id, maxValue, minValue, units, basemap, legendValues}, index) =>
-          id === 'land_cover.lccs_class' ? (
-            <HoverLegend
-              key={id}
-              values={legendValues as LegendValueColor[]}
-              isCompare={index > 0}
-            />
-          ) : (
-            <LayerLegend
-              key={id}
-              id={id}
-              values={
-                (legendValues as string[]) || [maxValue || 0, minValue || 0]
-              }
-              unit={units}
-              basemap={basemap}
-              isCompare={index > 0}
-            />
-          )
+        .map(
+          (
+            {id, maxValue, minValue, units, basemap, legendValues, hideLegend},
+            index
+          ) => {
+            if (hideLegend) {
+              return null;
+            }
+
+            return id === 'land_cover.lccs_class' ? (
+              <HoverLegend
+                key={id}
+                values={legendValues as LegendValueColor[]}
+                isCompare={index > 0}
+              />
+            ) : (
+              <LayerLegend
+                key={id}
+                id={id}
+                values={
+                  (legendValues as string[]) || [maxValue || 0, minValue || 0]
+                }
+                unit={units}
+                basemap={basemap}
+                isCompare={index > 0}
+              />
+            );
+          }
         )}
 
       {getDataWidget({
