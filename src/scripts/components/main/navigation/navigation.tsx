f6965f9790e421f17e8db8a7380f942e98bec2de
KatvonRivia <katvonrivia@gmail.com>
2022-03-07 16:39:27 +0100

    refactor: merge conflict fixes

--- src/scripts/components/main/navigation/navigation.tsx
+++ src/scripts/components/main/navigation/navigation.tsx
@@ -33,7 +33,7 @@ const Navigation: FunctionComponent = () => {
       <div className={styles.navigation}>
         <Button
           className={styles.button}
-          id="stories"
+          id="ui-stories"
           label="stories"
           link="/stories"
           icon={StoryIcon}
@@ -51,7 +51,7 @@ const Navigation: FunctionComponent = () => {
         )}
         <Button
           className={styles.button}
-          id="layers"
+          id="ui-layers"
           label="layers"
           onClick={() => dispatch(showLayerSelectorAction(true))}
           icon={LayersIcon}
@@ -60,7 +60,7 @@ const Navigation: FunctionComponent = () => {
         <Share />
         <Button
           className={styles.button}
-          id="menu"
+          id="ui-menu"
           icon={MenuIcon}
           onClick={() => setShowMenu(true)}
           hideLabelOnMobile
