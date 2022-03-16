a6df9d7eab94fddaa9a22a719fe7c4d156d8e629
Kat <45818654+KatvonRivia@users.noreply.github.com>
2022-03-16 09:43:15 +0100

    refactor(video): set video resolution (#1070)
    * feat(videoJs): implement videoJs to story video
    
    * refactor(video): add poster
    
    * refactor(video): set video resolution
    
    * refactor(story): change language
    
    * style: sort import

--- src/scripts/components/stories/video-js/video-js.tsx
+++ src/scripts/components/stories/video-js/video-js.tsx
@@ -3,12 +3,13 @@ import videojs, {VideoJsPlayer, VideoJsPlayerOptions} from 'video.js';
 import {getStoryAssetUrl} from '../../../libs/get-story-asset-urls';
 
 import {Language} from '../../../types/language';
+import {VideoResolution} from '../../../types/video-resolution-type';
 
 import 'video.js/dist/video-js.css';
 
 interface Props {
   storyId: string;
-  videoSrc: string;
+  videoSrc: string[];
   language: Language;
   isStoryMode: boolean;
   videoCaptions?: string;
@@ -27,7 +28,8 @@ const VideoJS: FunctionComponent<Props> = ({
 }) => {
   const videoRef = useRef(null);
   const playerRef = useRef<VideoJsPlayer | null>();
-  const videoUrl = videoSrc && getStoryAssetUrl(storyId, videoSrc);
+  const video = videoSrc[0];
+  const videoUrl = videoSrc && getStoryAssetUrl(storyId, video);
   const captionsUrl = videoCaptions && getStoryAssetUrl(storyId, videoCaptions);
   const posterUrl = videoPoster && getStoryAssetUrl(storyId, videoPoster);
 
@@ -54,8 +56,29 @@ const VideoJS: FunctionComponent<Props> = ({
     ]
   };
 
+  const getVideoResolution = (player: VideoJsPlayer, videoRes: string) => {
+    const currentResVideo = videoSrc.find(src => src.includes(videoRes));
+
+    if (currentResVideo) {
+      const mobileVideoUrl = getStoryAssetUrl(storyId, currentResVideo);
+      mobileVideoUrl && player.src(mobileVideoUrl);
+    }
+  };
+
   const handlePlayerReady = (player: VideoJsPlayer) => {
     playerRef.current = player;
+
+    const mobile = window.matchMedia('(max-width: 480px)');
+    const screenHD = window.matchMedia('(max-width: 1280px)');
+    const screen4k = window.matchMedia('(max-width: 3840px)');
+
+    if (mobile.matches) {
+      getVideoResolution(player, VideoResolution.SD576);
+    } else if (screenHD.matches) {
+      getVideoResolution(player, VideoResolution.HD720);
+    } else if (screen4k.matches) {
+      getVideoResolution(player, VideoResolution.HD1080);
+    }
   };
 
   useEffect(() => {
@@ -72,6 +95,7 @@ const VideoJS: FunctionComponent<Props> = ({
         () => handlePlayerReady(player)
       ));
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [videoJsOptions, videoRef]);
 
   // Dispose the Video.js player when component unmounts
