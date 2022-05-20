262d3f7f2535ce756bcc6ba5e5c977251606f3cf
Philipp Wambach <philippwambach@googlemail.com>
2022-05-20 08:41:59 +0200

    feat: update dataset soil moisture to v6.1

--- data/triggers/soil_moisture_sm.sh
+++ data/triggers/soil_moisture_sm.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 TIMEOUT=8000
-LAYER_ID="soil_moisture.sm_mean"
-VARIABLE_ID="sm_mean"
+LAYER_ID="soil_moisture.sm"
+VARIABLE_ID="sm"
 LAYER_TYPE="image"
 VERSION="1.5.1"
 LON_RES="1440"
