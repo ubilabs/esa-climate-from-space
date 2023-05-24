d37e3d9a9567c8ec692f2cf2acdd32dbbb56d9d1
Philipp Wambach <philippwambach@googlemail.com>
2023-05-24 14:37:26 +0200

    fix(globe): fix flyTo in story mode

--- src/scripts/hooks/use-slide.ts
+++ src/scripts/hooks/use-slide.ts
@@ -9,6 +9,16 @@ import setGlobeTimeAction from '../actions/set-globe-time';
 import {Slide} from '../types/story';
 import {CameraView, RenderMode} from '@ubilabs/esa-webgl-globe';
 
+function flyToToCameraView(flyTo: Slide['flyTo']): CameraView {
+  return {
+    renderMode: 'globe' as RenderMode.GLOBE,
+    lng: flyTo.position.longitude,
+    lat: flyTo.position.latitude,
+    altitude: flyTo.position.height,
+    zoom: 0
+  };
+}
+
 export const useSlide = (slide: Slide) => {
   const dispatch = useDispatch();
   const defaultView = config.globe.view;
@@ -22,13 +32,9 @@ export const useSlide = (slide: Slide) => {
       : 0;
 
     // FIXME: the stories are the last place where the old flyTo syntax is being used.
-    const cameraView: CameraView = {
-      renderMode: 'globe' as RenderMode,
-      lat: slide.flyTo.position.latitude,
-      lng: slide.flyTo.position.longitude,
-      altitude: slide.flyTo.position.height,
-      zoom: 0
-    };
+    const cameraView: CameraView =
+      slide.flyTo && flyToToCameraView(slide.flyTo);
+
     dispatch(setFlyToAction(cameraView || defaultView));
     dispatch(setSelectedLayerIdsAction(mainLayer?.id || null, true));
     dispatch(setSelectedLayerIdsAction(compareLayer?.id || null, false));
