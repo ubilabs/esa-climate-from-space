10cccacbe9522e53f9e86e1baa7df881518b7fce
Martin Schuhfuss <m.schuhfuss@gmail.com>
2023-06-02 10:44:28 +0200

    feat(globe): migrate to custom globe implementation (#1236)
    * chore: update to vite 4
    
    * chore: update typescript 4 -> 5
    
    * chore: update electron to 24.2.0
    
    * chore: update node typings
    
    * feat: initial working version of webgl-globe
    
    * refactor: restructure react-hooks for globe
    
    * feat(webgl-globe): add projection switcher
    
    * feat(webgl-globe)!: remove cesium and related code
    
     - removes cesium dependency
     - remove old code referencing the cesium globe
     - replaced cesium specific GlobeView type with CameraView type from @ubilabs/esa-webgl-globe
    
    * feat(webgl-globe): synchronized globes in compare-mode
    
    * fix(webgl-globe): remove remnants of old GlobeView type
    
    * feat(marker): add hover effect
    
    * fix(globe): fix globe resize on compare
    
    * fix(globe): update for marker click fix
    
    * fix(globe): fix flyTo in story mode
    
    * fix: update dependencies, triggering resize-events is no longer needed
    
    * feat(webgl-globe): implement moveStart/moveEnd callbacks via timers
    
    * chore: npm update
    
    * fix: rewrite url parsing and serialization
    
    * fix: use initial view-state when creating the globe instance.
    
    * fix(layers): wait for layer-details before selecting layer
    
    If we set the selected layer before the details are available, the globe will - for a few frames - render the default globe, which leads to flickering.
    
    ---------
    
    Co-authored-by: Philipp Wambach <philippwambach@googlemail.com>

--- src/scripts/reducers/globe/view.ts
+++ src/scripts/reducers/globe/view.ts
@@ -2,16 +2,16 @@ import {SET_GLOBE_VIEW, SetGlobeViewAction} from '../../actions/set-globe-view';
 import {parseUrl} from '../../libs/globe-url-parameter';
 import config from '../../config/main';
 
-import {GlobeView} from '../../types/globe-view';
+import {CameraView} from '@ubilabs/esa-webgl-globe';
 
 // get initial state from url or fallback to default state in config
 const globeState = parseUrl()?.globeState || config.globe;
 const initialState = globeState.view;
 
 function globeViewReducer(
-  state: GlobeView = initialState,
+  state: CameraView = initialState,
   action: SetGlobeViewAction
-): GlobeView {
+): CameraView {
   switch (action.type) {
     case SET_GLOBE_VIEW:
       return action.view;
