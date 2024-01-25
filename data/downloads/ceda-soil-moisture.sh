e80a8ad5173dfaa701628cdfdc8b688a948f6400
andreashelms <helms@ubilabs.net>
2024-01-25 15:49:29 +0100

    pipeline: update soil moisture

--- data/downloads/ceda-soil-moisture.sh
+++ data/downloads/ceda-soil-moisture.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 BASE_URL="https://data.ceda.ac.uk/neodc/esacci/soil_moisture/data/daily_files/COMBINED/v08.1"
-          
+
 
 START_DATE=1978-11-01
 OUTPUT_FOLDER=./downloads/download/soilmoisture
 
 mkdir -p $OUTPUT_FOLDER
 
-for i in {0..530}
+for i in {0..529}
 do
   NEXT_YEAR=$(date +%Y -d "$START_DATE + $i month")
   NEXT_DATE_SPACE=$(date +%Y%m%d -d "$START_DATE + $i month")
