3df3218aeda76f993945f5083d61866a34a3a5fe
andreashelms <helms@ubilabs.net>
2024-01-31 16:31:25 +0100

    feat(pipeline): add land surface temperature

--- data/downloads/odp-ftp-land_surface_temperature.sh
+++ data/downloads/odp-ftp-land_surface_temperature.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-BASE_URL="ftp://anon-ftp.ceda.ac.uk/neodc/esacci/land_surface_temperature/data/SSMI_SSMIS/L3C/v2.23//monthly/"
+BASE_URL="ftp://anon-ftp.ceda.ac.uk/neodc/esacci/land_surface_temperature/data/SSMI_SSMIS/L3C/v2.33//monthly/"
 START_DATE=1996-01-01
 OUTPUT_FOLDER=./download/lst
 
@@ -11,7 +11,7 @@ do
   NEXT_DATE=$(date +%Y%m -d "$START_DATE + $i month")
   NEXT_YEAR=$(date +%Y -d "$START_DATE + $i month")
   FILENAME=$OUTPUT_FOLDER/$(date +%Y%m%d -d "$START_DATE + $i month").nc
-  FTP_URL=$BASE_URL$NEXT_YEAR/"ESACCI-LST-L3C-LST-SSMI13-0.25deg_1MONTHLY_ASC-"$NEXT_DATE"000000-fv2.23.nc"
+  FTP_URL=$BASE_URL$NEXT_YEAR/"ESACCI-LST-L3C-LST-SSMI13-0.25deg_1MONTHLY_ASC-"$NEXT_DATE"000000-fv2.33.nc"
   echo $FTP_URL
 
   curl --silent $FTP_URL > $FILENAME
@@ -23,7 +23,7 @@ do
   NEXT_DATE=$(date +%Y%m -d "$START_DATE + $i month")
   NEXT_YEAR=$(date +%Y -d "$START_DATE + $i month")
   FILENAME=$OUTPUT_FOLDER/$(date +%Y%m%d -d "$START_DATE + $i month").nc
-  FTP_URL=$BASE_URL$NEXT_YEAR/"ESACCI-LST-L3C-LST-SSMI17-0.25deg_1MONTHLY_ASC-"$NEXT_DATE"000000-fv2.23.nc"
+  FTP_URL=$BASE_URL$NEXT_YEAR/"ESACCI-LST-L3C-LST-SSMI17-0.25deg_1MONTHLY_ASC-"$NEXT_DATE"000000-fv2.33.nc"
   echo $FTP_URL
 
   curl --silent $FTP_URL > $FILENAME
