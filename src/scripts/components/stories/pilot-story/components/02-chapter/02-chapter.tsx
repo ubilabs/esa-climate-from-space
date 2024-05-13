94cf7cde6ffcfa5c08f6395b83dcf5edd8a05310
Andreas Helms <andreashelms@MBP-von-Andreas.fritz.box>
2024-05-13 17:29:31 +0200

    feat(pilot-story): add remaining pages of chapter 2

--- src/scripts/components/stories/pilot-story/components/02-chapter/02-chapter.tsx
+++ src/scripts/components/stories/pilot-story/components/02-chapter/02-chapter.tsx
@@ -3,8 +3,10 @@ import {useHistory} from 'react-router-dom';
 import {Parallax} from 'react-scroll-parallax';
 
 import ChapterIntro from '../chapter-intro/chapter-intro';
+import ChapterText from '../chapter-text/chapter-text';
 
 import PilotCarousel from '../pilot-carousel/pilot-carousel';
+import ExploreDataset from '../explore-dataset/explore-dataset';
 
 import styles from './02-chapter.module.styl';
 
@@ -31,10 +33,15 @@ const ChapterTwo: FunctionComponent<Props> = ({
           onEnter={() => setEntered(true)}
           onExit={() => setEntered(false)}>
           <ChapterIntro
-            subTitle="Chapter 2: What is methane"
+            subTitle="Chapter 2: Methane Sources"
             title="Where does methane come from?"
           />
+          <ChapterText text="Methane emissions arise from a multitude of sources, spanning both natural processes and human activities." />
+          <ChapterText text="These sources contribute to the complex dynamics of methane concentrations in the atmosphere, playing a significant role in global climate change." />
+          <ChapterText text="Understanding where methane comes from is essential for creating effective ways to reduce its harm to the environment and human health." />
           <PilotCarousel />
+          <ExploreDataset />
+          <ChapterText text="" />
         </Parallax>
       </section>
     </>
