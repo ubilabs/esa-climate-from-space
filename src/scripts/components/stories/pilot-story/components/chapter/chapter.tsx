d002de35665812a9878912c41c7440a4d2cf01df
Andreas Helms <andreashelms@MBP-von-Andreas.fritz.box>
2024-05-22 09:55:00 +0200

    feat(pilot-story-globe): set data layer per chapter

--- src/scripts/components/stories/pilot-story/components/chapter/chapter.tsx
+++ src/scripts/components/stories/pilot-story/components/chapter/chapter.tsx
@@ -1,12 +1,21 @@
-import React, {FunctionComponent, PropsWithChildren} from 'react';
+import React, {FunctionComponent, PropsWithChildren, useEffect} from 'react';
 import {Parallax, ParallaxProps} from 'react-scroll-parallax';
 
 import cx from 'classnames';
 
 import {useChapter} from '../../hooks/use-chapter';
+import {useGlobeLayers} from '../../hooks/use-globe';
+
+import {
+  BLUE_BASE_MAP,
+  GREENHOUSE_XCH4,
+  GREY_BASE_MAP,
+  SUPER_EMITTERS,
+  chapterMainElement
+} from '../../config/main';
 
 import styles from './chapter.module.styl';
-import {chapterMainElement} from '../../config/main';
+
 interface Props
   extends PropsWithChildren<React.JSX.IntrinsicElements['section']> {
   scrollIndex: number;
@@ -31,7 +40,40 @@ const Chapter: FunctionComponent<Props> = ({
   parallaxProps,
   ...rest
 }) => {
-  const {onSetProgress} = useChapter();
+  const {setLayers} = useGlobeLayers();
+  const {onSetProgress, selectedChapterIndex} = useChapter();
+
+  useEffect(() => {
+    switch (selectedChapterIndex) {
+      case 0:
+        setLayers([GREY_BASE_MAP, GREENHOUSE_XCH4]);
+        break;
+      case 1:
+        setLayers([GREY_BASE_MAP, GREENHOUSE_XCH4]);
+        break;
+      case 2:
+        setLayers([GREY_BASE_MAP]);
+        break;
+      case 3:
+        setLayers([BLUE_BASE_MAP]);
+        break;
+      case 4:
+        setLayers([BLUE_BASE_MAP]);
+        break;
+      case 5:
+        setLayers([GREY_BASE_MAP, SUPER_EMITTERS]);
+        break;
+      case 6:
+        setLayers([BLUE_BASE_MAP]);
+        break;
+      case 7:
+        setLayers([BLUE_BASE_MAP]);
+        break;
+      default:
+        break;
+    }
+  }, [selectedChapterIndex, setLayers]);
+
   return (
     <section
       className={cx(styles.sectionContainer, className)}
