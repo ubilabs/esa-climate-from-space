3b53f95c181f334f1de5396d733f02e29169e906
immobe <i.beeck@gmail.com>
2024-05-29 14:54:34 +0200

    feat(chapter-06): add select giant hint

--- src/scripts/components/stories/pilot-story/components/scroll-hint/scroll-hint.tsx
+++ src/scripts/components/stories/pilot-story/components/scroll-hint/scroll-hint.tsx
@@ -5,13 +5,31 @@ import {ScrollIcon} from '../icons/scroll-icon/scroll-icon';
 
 import styles from './scroll-hint.module.styl';
 import {useScreenSize} from '../../../../../hooks/use-screen-size';
+import TouchIcon from '../icons/touch-icon/touch-icon';
 
-const ScrollHint: FunctionComponent = () => {
+interface Props {
+  isSelectHint?: boolean;
+}
+
+const ScrollHint: FunctionComponent<Props> = ({isSelectHint = false}) => {
   const {isDesktop} = useScreenSize();
+
+  const hint = isSelectHint
+    ? 'Select a methane giant'
+    : 'Scroll to discover more';
+  // eslint-disable-next-line no-nested-ternary
+  const icon = isSelectHint ? (
+    <TouchIcon />
+  ) : isDesktop ? (
+    <ScrollIcon />
+  ) : (
+    <ExpandIcon className={styles.animated} />
+  );
+
   return (
     <div className={styles.scrollHint}>
-      {isDesktop ? <ScrollIcon /> : <ExpandIcon className={styles.animated} />}
-      Scroll to discover more
+      {icon}
+      {hint}
     </div>
   );
 };
