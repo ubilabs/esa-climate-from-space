e0f8b8a0c249abf12ac3c6812ba7aa20e06ec9ac
Andreas Helms <andreashelms@MacBook-Pro-von-Andreas.local>
2024-09-17 16:06:37 +0200

    feat(pipeline): river discharge preprocessing and dag

---
+++ data/river-discharge/create-raster.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+from datetime import datetime
+
+# Load GeoDataFrame
+gdf = gpd.read_file('rivers.geojson')
+
+# Convert timestamps and calculate buffer sizes
+gdf['timestamp'] = pd.to_datetime(gdf['timestamp'], format='%Y-%m-%d')
+
+bufferSize = np.where(gdf['average_value'].abs() > 2000, 
+                         1000000, 
+                         gdf['average_value'].abs() * 500)
+
+gdf['buffer_size'] = bufferSize
+gdf = gdf.to_crs(epsg=32633)  # Adjust CRS to one that uses meters for buffer calculations
+gdf['geometry'] = gdf.geometry.buffer(gdf['buffer_size'])
+
+# Filter data by month and store each month's data in a separate layer in a GeoPackage
+start_date = datetime(2010, 1, 1)
+end_date = datetime(2023, 12, 31)
+current_date = start_date
+while current_date <= end_date:
+    print(current_date)
+    # Filter data for the current month
+    month_data = gdf[(gdf['timestamp'].dt.year == current_date.year) &
+                     (gdf['timestamp'].dt.month == current_date.month)]
+    if not month_data.empty:
+        # Convert to WGS84 before saving
+        month_data = month_data.to_crs(epsg=4326)
+        # Define the layer name as 'data_YEAR_MONTH' (e.g., 'data_1993_01')
+        layer_name = f"data_{current_date.year}_{current_date.month:02d}"
+        # Write each month's data to a separate layer in the GeoPackage
+        month_data.to_file("monthly_data.gpkg", layer=layer_name, driver="GPKG")
+
+    # Increment to the next month
+    next_month = current_date.month % 12 + 1
+    next_year = current_date.year if next_month > 1 else current_date.year + 1
+    current_date = datetime(next_year, next_month, 1)
