cac92fe2fa5482c64b985df5b4aace9d49f011d0
immobe <i.beeck@gmail.com>
2025-02-20 16:50:20 +0100

    fix: update import path for GlobeState in main.ts

--- src/scripts/config/main.ts
+++ src/scripts/config/main.ts
@@ -1,9 +1,9 @@
 import { RenderMode } from "@ubilabs/esa-webgl-globe";
 
-import { GlobeState } from "../reducers/globe/index";
 import { UiEmbedElement } from "../types/embed-elements";
 
 import { GlobeProjection } from "../types/globe-projection";
+import { GlobeState } from "../reducers/globe/globe-state";
 
 const globeState: GlobeState = {
   time: Date.now(),
