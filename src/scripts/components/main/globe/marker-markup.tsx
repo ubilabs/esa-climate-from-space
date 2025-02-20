81222125ce300592b3a3f5e72d45cd932db1403c
immobe <i.beeck@gmail.com>
2025-02-20 16:48:43 +0100

    refactor: replace getMarkerHtml with MarkerMarkup component for improved React integration

---
+++ src/scripts/components/main/globe/marker-markup.tsx
@@ -0,0 +1,25 @@
+import { FunctionComponent } from "react";
+
+import styles from './globe.module.css';
+
+interface Props {
+  title: string;
+}
+// This is rendered to markup using React.renderToStaticMarkup
+// So make sure not to use any state or hooks here
+export const MarkerMarkup: FunctionComponent<Props> = ({ title }) => {
+  return (
+    <svg
+      className={styles.marker}
+      data-marker={title}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <circle cx="12" cy="12" r="5" stroke="#00B398" />
+      <circle cx="12" cy="12" r="2" fill="#00B398" />
+    </svg>
+  );
+};
