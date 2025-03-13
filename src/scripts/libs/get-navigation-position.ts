bd9fd58e6599e948826729d7d940e4cd1a487c81
immobe <i.beeck@gmail.com>
2025-03-13 10:12:30 +0100

    chore: add comment to clarify calculation

--- src/scripts/libs/get-navigation-position.ts
+++ src/scripts/libs/get-navigation-position.ts
@@ -23,5 +23,6 @@ export function getNavCoordinates(
   const angleInDegrees = pos * gap;
   const { x, y } = computeCartesianCoordinates(radius, angleInDegrees);
 
+  // The calcuated x and y position will essentially be used as the top and left position of the absoluted positioned content entries within the content list container. By adjusting the y coordinate by 50 we vertically center the items, making sure the item in the middle is placed in center of the list
   return { x: x - (isMoble ? 0 : radius), y: y + 50 };
 }
