7aec84d410fa8475bd6c01958d823bc5dfd4798a
immobe <i.beeck@gmail.com>
2025-03-19 19:09:17 +0100

    fix: fix spelling for svg attributes

--- src/scripts/components/main/icons/menu-icon.tsx
+++ src/scripts/components/main/icons/menu-icon.tsx
@@ -9,8 +9,8 @@ export const MenuIcon: FunctionComponent = () => (
     fill="none"
   >
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M3 8V6H21V8H3ZM3 13H21V11H3V13ZM3 18H21V16H3V18Z"
       fill="white"
     />
