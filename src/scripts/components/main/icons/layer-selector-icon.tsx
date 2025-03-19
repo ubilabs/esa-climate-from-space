f48c888caa6dde3ffd2e9a6f6f3c69244cd5e0fd
immobe <i.beeck@gmail.com>
2025-03-19 11:19:06 +0100

    feat: add layer selector icon

---
+++ src/scripts/components/main/icons/layer-selector-icon.tsx
@@ -0,0 +1,16 @@
+import { FunctionComponent } from "react";
+
+export const LayerSelectorIcon: FunctionComponent = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+  >
+    <path
+      d="M10 23V21H5C4.45 21 3.97917 20.8042 3.5875 20.4125C3.19583 20.0208 3 19.55 3 19V5C3 4.45 3.19583 3.97917 3.5875 3.5875C3.97917 3.19583 4.45 3 5 3H10V1H12V23H10ZM5 18H10V12L5 18ZM14 21V12L19 18V5H14V3H19C19.55 3 20.0208 3.19583 20.4125 3.5875C20.8042 3.97917 21 4.45 21 5V19C21 19.55 20.8042 20.0208 20.4125 20.4125C20.0208 20.8042 19.55 21 19 21H14Z"
+      fill="white"
+    />
+  </svg>
+)
