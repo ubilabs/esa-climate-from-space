d839a34849faa677c89f6d1699ed77b1a9cd3cbc
immobe <i.beeck@gmail.com>
2025-06-13 13:11:22 +0200

    fix(url-sync): fixes and refactors url sync component. This fix makes sure that the location.pathname changes are applied only once, avoiding unnessary re-renders and action dipsatches

--- src/scripts/components/main/url-sync/url-sync.tsx
+++ src/scripts/components/main/url-sync/url-sync.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, useEffect } from "react";
+import { FunctionComponent, useEffect, useMemo } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 
@@ -11,111 +11,75 @@ import { embedElementsSelector } from "../../../selectors/embed-elements-selecto
 import { uiEmbedElements } from "../../../config/main";
 import { getEmbedParamsString } from "../../../libs/get-embed-params-string";
 
-// syncs the query parameters of the url when values change in store
 const UrlSync: FunctionComponent = () => {
   const navigate = useNavigate();
   const location = useLocation();
+
   const globeState = useSelector(globeStateSelector);
   const selectedTags = useSelector(selectedTagsSelector);
   const embedElements = useSelector(embedElementsSelector);
   const { mainId, compareId } = useSelector(selectedLayerIdsSelector);
 
-  const globeParamString = getGlobeParamString(globeState, mainId, compareId);
-  const tagsParamString = getTagsParamString(selectedTags);
-  const embedParamString = getEmbedParamsString(embedElements);
-  const embedParamOptions: string[] = uiEmbedElements.reduce<string[]>(
-    (accumulator, currentValue) => accumulator.concat(currentValue.elements),
+  const embedParamOptions = useMemo(
+    () =>
+      uiEmbedElements.reduce<string[]>(
+        (acc, el) => acc.concat(el.elements),
+        [],
+      ),
     [],
   );
 
-  // set globe query params in url when globe state changes
+  const targetSearch = useMemo(() => {
+    const params = new URLSearchParams();
+
+    const globeParam = getGlobeParamString(globeState, mainId, compareId);
+    if (globeParam) params.set("globe", globeParam);
+
+    const tagsParam = getTagsParamString(selectedTags);
+    if (tagsParam) params.set("tags", tagsParam);
+
+    const embedParam = getEmbedParamsString(embedElements);
+    if (embedParam) {
+      embedParam.split("&").forEach((kv) => {
+        const [key, value] = kv.split("=");
+        if (key && value) params.set(key, value);
+      });
+    }
+
+    return params.toString();
+  }, [globeState, mainId, compareId, selectedTags, embedElements]);
+
   useEffect(() => {
-    if (!globeParamString) {
-      return;
+    const currentParams = new URLSearchParams(location.search);
+    const targetParams = new URLSearchParams(targetSearch);
+
+    let hasChanges = false;
+
+    // compare keys
+    for (const [key, value] of targetParams.entries()) {
+      if (currentParams.get(key) !== value) {
+        hasChanges = true;
+        break;
+      }
     }
 
-    const params = new URLSearchParams(location.search);
-    params.set("globe", globeParamString);
-    navigate({ search: params.toString() }, { replace: true });
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [globeState, mainId, compareId, navigate]); // we don't want to check for location.search changes
-
-  // set story tag query params in url when tags state changes
-  useEffect(() => {
-    const params = new URLSearchParams(location.search);
-
-    if (!tagsParamString) {
-      params.delete("tags");
-    } else {
-      params.set("tags", tagsParamString);
+    // check for stale keys that should be removed
+    if (!hasChanges) {
+      for (const key of currentParams.keys()) {
+        if (
+          !targetParams.has(key) &&
+          (key === "globe" || key === "tags" || embedParamOptions.includes(key))
+        ) {
+          hasChanges = true;
+          break;
+        }
+      }
     }
 
-    navigate({ search: params.toString() }, { replace: true });
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [selectedTags, navigate]); // we don't want to check for location.search changes
-
-  // set embed elements query params in url when state changes
-  useEffect(() => {
-    const params = new URLSearchParams(location.search);
-
-    if (!embedParamString) {
-      embedParamOptions.forEach((option) => params.delete(option));
-    } else {
-      const paramsArray = embedParamString
-        .split("&")
-        .map((key) => key.split("="));
-
-      paramsArray.map((param) => params.set(param[0], param[1]));
+    if (hasChanges) {
+      navigate({ search: targetSearch }, { replace: true });
     }
-
-    navigate({ search: params.toString() }, { replace: true });
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [embedElements, navigate]); // we don't want to check for location.search changes
-
-  // listen on location updates and re-add query parameters if missing
-  useEffect(() => {
-    const params = new URLSearchParams(location.search);
-
-    let updated = false;
-
-    const hasEmbedParam = embedParamOptions.some((element) =>
-      params.has(element),
-    );
-
-    if (tagsParamString && params.get("tags") !== tagsParamString) {
-      params.set("tags", tagsParamString);
-      updated = true;
-    }
-
-    if (globeParamString && !params.has("globe")) {
-      params.set("globe", globeParamString);
-      updated = true;
-    }
-
-    if (embedParamString && !hasEmbedParam) {
-      const paramsArray = embedParamString
-        .split("&")
-        .map((key) => key.split("="));
-
-      paramsArray.forEach(([key, value]) => params.set(key, value));
-      updated = true;
-    }
-
-    if (updated) {
-      navigate({ search: params.toString() }, { replace: true });
-    }
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [
-    location.search,
-    tagsParamString,
-    globeParamString,
-    embedParamString,
-    navigate,
-  ]);
+  }, [targetSearch, location.search, navigate, embedParamOptions]);
 
   return null;
 };
