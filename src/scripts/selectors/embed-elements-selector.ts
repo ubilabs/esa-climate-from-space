73444ffe867003a47c11cb617d83d1782fd716f6
immobe <i.beeck@gmail.com>
2025-06-27 18:04:03 +0200

    fix(embed-selector): remove createSelector as it is not need

--- src/scripts/selectors/embed-elements-selector.ts
+++ src/scripts/selectors/embed-elements-selector.ts
@@ -1,8 +1,3 @@
-import { createSelector } from "@reduxjs/toolkit";
 import { State } from "../reducers";
-import { EmbedElementsState } from "../types/embed-elements";
 
-export const embedElementsSelector = createSelector(
-  (state: State) => state.embedElements,
-  (embedElements: EmbedElementsState) => embedElements
-);
+export const embedElementsSelector = (state: State) => state.embedElements;
