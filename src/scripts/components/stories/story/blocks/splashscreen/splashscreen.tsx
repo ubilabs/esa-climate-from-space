19480afc98c876b3216a957c1d200ad0255f991a
immobe <i.beeck@gmail.com>
2025-07-09 16:40:56 +0200

    feat(splashscreen): add parallax splashscreen

--- src/scripts/components/stories/story/blocks/splashscreen/splashscreen.tsx
+++ src/scripts/components/stories/story/blocks/splashscreen/splashscreen.tsx
@@ -1,10 +1,14 @@
 import { FunctionComponent } from "react";
 
-import styles from "./splashscreen.module.css";
 import { getStoryAssetUrl } from "../../../../../libs/get-story-asset-urls";
 import { useStory } from "../../../../../providers/story/use-story";
+import ReactMarkdown from "react-markdown";
 import { FormatParallexLayout } from "../../../layout/block-format-layout/block-format-section";
 import { StorySectionProps } from "../../../../../types/story";
+
+import { ParallaxBanner } from "react-scroll-parallax";
+
+import styles from "./splashscreen.module.css";
 
 export const SplashScreen: FunctionComponent<StorySectionProps> = ({
   slideIndex,
@@ -21,8 +25,23 @@ export const SplashScreen: FunctionComponent<StorySectionProps> = ({
   const imageUrl = getStoryAssetUrl(id, image);
   return (
     <FormatParallexLayout className={styles.splashscreen} index={slideIndex}>
-      <img src={imageUrl} />
-      <div className={styles.textContainer}>{text}</div>
+      <ParallaxBanner
+        layers={[
+          { image: imageUrl, speed: -10, className: styles.parallaxContainer },
+          {
+            speed: -30,
+            style: { display: "grid", placeItems: "center" },
+            children: (
+              <ReactMarkdown
+                className={styles.content}
+                children={text}
+                allowedElements={["h1", "h2", "h3", "p", "br", "em", "b"]}
+              />
+            ),
+          },
+        ]}
+        className={styles.splashscreen}
+      ></ParallaxBanner>
     </FormatParallexLayout>
   );
 };
