02d035dbeb7e0c6f7d2ef08bbec3ba23fce8296c
immobe <i.beeck@gmail.com>
2025-07-18 12:33:44 +0200

    feat(scroll-caption): add scroll caption component

--- src/scripts/components/stories/story/blocks/image-gallery/formats/scroll-caption/scroll-caption.tsx
+++ src/scripts/components/stories/story/blocks/image-gallery/formats/scroll-caption/scroll-caption.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import { FunctionComponent } from "react";
+import { useFormat } from "../../../../../../../providers/story/format/use-format";
+import { FormatContainer } from "../../../../../layout/format-container/format-container";
+import { StorySectionProps } from "../../../../../../../types/story";
 
-const ScrollCaption: FunctionComponent = () => {
-  return (
-    <section>
-      Scroll Caption Component
-    </section>
-  );
+const ScrollCaption: FunctionComponent<StorySectionProps> = ({ ref }) => {
+  const { content, storyId } = useFormat();
+  console.log("ScrollCaption content:", content, "storyId:", storyId);
+
+  return <FormatContainer ref={ref}>Scroll Caption Component</FormatContainer>;
 };
 
 export default ScrollCaption;
-
