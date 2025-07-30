14e45c6e7002496dd0e398afc742b24ee87f77dc
immobe <i.beeck@gmail.com>
2025-07-30 15:53:02 +0200

    wip

--- src/scripts/components/stories/story/blocks/image-gallery/formats/scroll-overlay/scroll-overlay-slide/scroll-overlay-slide.tsx
+++ src/scripts/components/stories/story/blocks/image-gallery/formats/scroll-overlay/scroll-overlay-slide/scroll-overlay-slide.tsx
@@ -1,13 +1,79 @@
 import { FunctionComponent, useRef } from "react";
-
 import cx from "classnames";
-
-import { motion, useTransform } from "motion/react";
+import {
+  motion,
+  useTransform,
+  MotionValue,
+  useMotionValueEvent,
+} from "motion/react";
 import { getStoryAssetUrl } from "../../../../../../../../libs/get-story-asset-urls";
 import { useStoryScroll } from "../../../../../../../../hooks/use-story-scroll";
 import { ImageSlide } from "../../../../../../../../types/story";
-
 import styles from "./scroll-overlay-slide.module.css";
+
+interface CaptionProps {
+  caption: string;
+  subCaption?: string;
+  scrollYProgress: MotionValue<number>;
+  index: number;
+  totalCaptions: number;
+}
+
+const Caption: FunctionComponent<CaptionProps> = ({
+  caption,
+  subCaption,
+  index,
+  totalCaptions,
+}) => {
+  // const numSteps = totalCaptions > 1 ? totalCaptions + 1 : totalCaptions;
+  // const step = 1 / numSteps;
+  // const start = step * index;
+  // const end = start + step;
+
+  const ref = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useStoryScroll({
+    target: ref,
+    offset: ["start end", "end start"],
+  });
+
+  console.log("index", index, "scrollprocess", scrollYProgress);
+  useMotionValueEvent(scrollYProgress, "change", (e) => {
+    if (index === 1) {
+      console.log("e", e);
+    }
+  });
+  // const numSteps = totalCaptions > 1 ? totalCaptions + 1 : totalCaptions;
+  // const step = 1 / numSteps;
+  // const start = 0.5 - step / 2 + step * index;
+  // const end = start + step;
+
+
+  const captionOpacity = useTransform(
+    scrollYProgress,
+    [0,1],
+    [0, 1],
+  );
+
+  const captionTransform = useTransform(
+    scrollYProgress,
+    [0,1],
+    ["100px", "-100px"], // symmetrical vertical movement
+  );
+  return (
+    <motion.div
+      ref={ref}
+      style={{
+        backgroundColor: `rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.random()})`,
+        opacity: captionOpacity,
+        x: captionTransform,
+      }}
+      className={cx(styles.captionContainer)}
+    >
+      <h2>{caption}</h2>
+      {subCaption && <h3>{subCaption}</h3>}
+    </motion.div>
+  );
+};
 
 interface Props {
   slide: ImageSlide;
@@ -22,58 +88,42 @@ export const ScrollOverlaySlide: FunctionComponent<Props> = ({
   slide,
   storyId,
 }) => {
-  const ref = useRef<HTMLDivElement>(null);
-  const assetUrl = getStoryAssetUrl(storyId, slide.url);
-
-  const { scrollYProgress } = useStoryScroll({
-    target: ref,
-    offset: ["start end", "end start"],
-  });
-
-  const captionOpacity = useTransform(
-    scrollYProgress,
-    [0, 0.3, 0.7, 1],
-    [0, 1, 1, 0],
-  );
-
-  const captionTransform = useTransform(
-    scrollYProgress,
-    [0, 0.5, 1],
-    ["500px", "0px", "500px"],
-  );
+  const assetUrl = getStoryAssetUrl(storyId, slide?.url);
+  const hasAsset = assetUrl && assetUrl.length > 0;
 
   return (
-    <div ref={ref} className={cx(styles.slide)}>
+    <div
+      className={cx(styles.slide)}
+      style={{ height: `calc(2 * var(--story-height))` }}
+    >
       <div className={styles.assetContainer}>
-        {isVideo(slide.url) ? (
-          <video
-            className={styles.asset}
-            src={assetUrl}
-            autoPlay
-            muted
-            loop
-            playsInline
-          />
-        ) : (
-          <img
-            className={styles.asset}
-            src={assetUrl}
-            alt={slide.altText || ""}
-          />
-        )}
+        {hasAsset &&
+          (isVideo(slide.url) ? (
+            <video
+              className={styles.asset}
+              src={assetUrl}
+              autoPlay
+              muted
+              loop
+              playsInline
+            />
+          ) : (
+            <img
+              className={styles.asset}
+              src={assetUrl}
+              alt={slide.altText || ""}
+            />
+          ))}
       </div>
-      <motion.div
-        style={{
-          opacity: captionOpacity,
-          x: captionTransform,
-        }}
-        className={cx(styles.captionContainer)}
-      >
-        <>
-          <h2>{slide.caption}</h2>
-          {slide.subCaption && <h3>{slide.subCaption}</h3>}
-        </>
-      </motion.div>
+      {slide.captions.map((caption, index) => (
+        <Caption
+          key={index}
+          caption={caption}
+          subCaption={slide.subCaption}
+          index={index}
+          totalCaptions={slide.captions.length}
+        />
+      ))}
     </div>
   );
 };
