a9ce805bd778f89fb4541e4ecfa2322805088792
immobe <i.beeck@gmail.com>
2025-08-22 14:38:48 +0200

    feat(blend): scroll page on x drag

--- src/scripts/components/stories/story/blocks/image-gallery/modules/shared/time-and-wavelength-blend-wrapper/time-and-wavelength-blend.tsx
+++ src/scripts/components/stories/story/blocks/image-gallery/modules/shared/time-and-wavelength-blend-wrapper/time-and-wavelength-blend.tsx
@@ -1,15 +1,23 @@
 import { useRef, useState, useMemo, FunctionComponent } from "react";
-import { useMotionValueEvent } from "motion/react";
+import { motion, useMotionValueEvent } from "motion/react";
+import { useGesture } from "@use-gesture/react";
 import {
   ImageModuleSlide,
   StorySectionProps,
 } from "../../../../../../../../types/story";
 
-import { AnimationDirection, TimeAndWavelengthBlendImage } from "../time-and-wavelength-blend-image/time-and-wavelength-blend-image";
+import {
+  AnimationDirection,
+  TimeAndWavelengthBlendImage,
+} from "../time-and-wavelength-blend-image/time-and-wavelength-blend-image";
 import { useStoryScroll } from "../../../../../../../../hooks/use-story-scroll";
 import { useModuleContent } from "../../../../../../../../providers/story/module-content/use-module-content";
+import { useStory } from "../../../../../../../../providers/story/use-story";
+import { useScreenSize } from "../../../../../../../../hooks/use-screen-size";
 
 import styles from "./time-and-wavelength-blend.module.css";
+
+const SENSITIVITY_FACTOR = 2.5;
 
 interface BlendWrapperProps extends StorySectionProps {
   animationDirection: AnimationDirection;
@@ -18,13 +26,18 @@ interface BlendWrapperProps extends StorySectionProps {
 const TimeAndWavelengthBlend: FunctionComponent<BlendWrapperProps> = ({
   animationDirection,
 }) => {
-  const { module, storyId , getRefCallback} = useModuleContent();
+  const { module, storyId, getRefCallback } = useModuleContent();
+  const { storyElementRef } = useStory();
+  const { isTouchDevice, isMobile } = useScreenSize();
 
   const targetRef = useRef<HTMLDivElement | null>(null);
-  const images: ImageModuleSlide[] = useMemo(() => module?.slides ?? [], [module]);
+  const images: ImageModuleSlide[] = useMemo(
+    () => module?.slides ?? [],
+    [module],
+  );
   const numSlides = images.length;
 
-  const [activeSlideIndex, setActiveSlideIndex] = useState<number | null>(null);
+  const [activeSlideIndex, setActiveSlideIndex] = useState<number>(0);
 
   const { scrollYProgress } = useStoryScroll({
     target: targetRef,
@@ -33,9 +46,35 @@ const TimeAndWavelengthBlend: FunctionComponent<BlendWrapperProps> = ({
 
   useMotionValueEvent(scrollYProgress, "change", (latest) => {
     setActiveSlideIndex(
-      Math.min(Math.round(latest * numSlides), numSlides - 1),
+      Math.min(Math.round(latest * (numSlides - 1)), numSlides - 1),
     );
   });
+
+  // Manually scroll on horizontal drag
+  // This will cause a warning in development about adding touch-action: none to the target element.
+  // However, we cannot do that because the element wouldn't be scrollable.
+  useGesture(
+    {
+      onDrag: ({ movement: [mx], first, memo }) => {
+        if (!storyElementRef.current) return memo;
+        if (first) {
+          memo = storyElementRef.current.scrollTop;
+        }
+        const newScrollTop = memo + mx * SENSITIVITY_FACTOR;
+        storyElementRef.current.scrollTop = newScrollTop;
+        return memo;
+      },
+    },
+    {
+      target: targetRef,
+      eventOptions: { passive: true },
+      drag: {
+        axis: "x",
+        filterTaps: true,
+        enabled: isTouchDevice && isMobile,
+      },
+    },
+  );
 
   const description = useMemo(() => {
     const activeSlide = images[activeSlideIndex ?? 0];
@@ -61,7 +100,7 @@ const TimeAndWavelengthBlend: FunctionComponent<BlendWrapperProps> = ({
       className={styles.stickySectionWrapper}
       style={{ height: `calc(${numSlides} * var(--story-height))` }}
     >
-      <div className={styles.stickyScroller}>
+      <motion.div className={styles.stickyScroller}>
         <ul className={styles.imageContainer}>
           {images.map((image, i) => (
             <TimeAndWavelengthBlendImage
@@ -81,7 +120,7 @@ const TimeAndWavelengthBlend: FunctionComponent<BlendWrapperProps> = ({
           <h3>{captions.join(" ")}</h3>
           <p>{description}</p>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
