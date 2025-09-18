60d854b9de3e00d1bb38aa8dd382f18736611cb0
immobe <i.beeck@gmail.com>
2025-09-18 12:01:26 +0200

    docs: update comment in is-legacy-story

--- src/scripts/libs/is-legacy-story.ts
+++ src/scripts/libs/is-legacy-story.ts
@@ -5,7 +5,7 @@ import { Story } from "../types/story";
  * Checks if a given story is a legacy story. Legacy stories are pagination-based stories
  */
 export const isLegacyStory = (story: LegacyStory | Story): boolean => {
-  // if a story has the new 'content' property, it is not a legacy story
+  // if a story has the new 'modules' property, it is not a legacy story
   if ("modules" in story) {
     return false;
   }
