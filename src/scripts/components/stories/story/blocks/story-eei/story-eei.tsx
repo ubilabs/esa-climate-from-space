4f5627922bac91b1293c1221a8896501189db306
immobe <i.beeck@gmail.com>
2026-02-25 14:23:58 +0100

    feat(story-eei): add storyglobe to compound

--- src/scripts/components/stories/story/blocks/story-eei/story-eei.tsx
+++ src/scripts/components/stories/story/blocks/story-eei/story-eei.tsx
@@ -2,10 +2,12 @@ import { FunctionComponent, ReactNode } from "react";
 
 import TextOverlay from "../generic/text-overlay/text-overlay";
 import BaseSlide from "./modules/base-slide/base-slide";
+import StoryGlobe from "../globe/story-globe/story-globe";
 
 export type StoryEEICompoundComponents = {
   TextOverlay: typeof TextOverlay;
   BaseSlide: typeof BaseSlide;
+  StoryGlobe: typeof StoryGlobe;
 };
 
 /* Module Wrapper for Earth Engine Imbalance Story Components*/
@@ -15,3 +17,4 @@ export const StoryEEI = (({ children }: { children: ReactNode }) => {
 
 StoryEEI.TextOverlay = TextOverlay;
 StoryEEI.BaseSlide = BaseSlide;
+StoryEEI.StoryGlobe = StoryGlobe;
