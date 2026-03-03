144220ba17925a091953e1b6232eee5756e13b8a
immobe <i.beeck@gmail.com>
2026-03-03 22:47:02 +0100

    feat(story-eei): implement scroll animations for kettle slide

--- src/scripts/hooks/use-lenis-for-story.ts
+++ src/scripts/hooks/use-lenis-for-story.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useCallback } from "react";
+import { useEffect, useRef, useCallback, useMemo } from "react";
 
 import Lenis from "lenis";
 import Snap from "lenis/snap";
