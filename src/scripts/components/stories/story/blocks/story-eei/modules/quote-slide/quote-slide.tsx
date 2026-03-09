9d04c531b55383b459464b05cc7f31c7c44051ed
immobe <i.beeck@gmail.com>
2026-03-09 15:28:45 +0100

    feat(story-eei): add scroll animations to quote slide

--- src/scripts/components/stories/story/blocks/story-eei/modules/quote-slide/quote-slide.tsx
+++ src/scripts/components/stories/story/blocks/story-eei/modules/quote-slide/quote-slide.tsx
@@ -1,20 +1,48 @@
+import { motion, useTransform } from "motion/react";
 import { useModuleContent } from "../../../../../../../providers/story/module-content/use-module-content";
 import { StoryEEIModule } from "../../../../../../../types/story";
 import ScrollModule from "../base-scroll/module/scroll-module";
+import { useScrollModule } from "../base-scroll/use-scroll-module";
 
 import styles from "./quote-slide.module.css";
 
-const animationConfig = {};
+const animationConfig = {
+  fadeIn: {
+    input: [0, 0.3, 0.7, 1],
+    output: [0, 1, 1, 0.8],
+  },
+};
 
-export default function QuoteSlide() {
+export type QuoteSlideAnimationConfig = typeof animationConfig;
+
+function QuoteContent() {
   const { module } = useModuleContent();
   const quoteModule = module as StoryEEIModule;
 
+  const { scrollYProgress, config } =
+    useScrollModule<QuoteSlideAnimationConfig>();
+
+  return (
+    <motion.div
+      style={{
+        opacity: useTransform(
+          scrollYProgress,
+          config.fadeIn.input,
+          config.fadeIn.output,
+        ),
+      }}
+    >
+      <p>{quoteModule.quote?.text}</p>
+      <span>{quoteModule.quote?.author}</span>
+    </motion.div>
+  );
+}
+
+export default function QuoteSlide() {
   return (
     <ScrollModule className={styles.quoteWrapper} config={animationConfig}>
       <ScrollModule.Slide className={styles.quote}>
-        <p>{quoteModule.quote?.text}</p>
-        <span>{quoteModule.quote?.author}</span>
+        <QuoteContent />
       </ScrollModule.Slide>
     </ScrollModule>
   );
