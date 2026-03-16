9deaf6a39a7aaa739c592b193bb09a8eaec39f95
immobe <i.beeck@gmail.com>
2026-03-16 09:01:24 +0100

    feat(splash): add story-eei-splash

--- src/scripts/components/stories/story/blocks/splashscreen/splashscreen-eei.tsx
+++ src/scripts/components/stories/story/blocks/splashscreen/splashscreen-eei.tsx
@@ -26,12 +26,58 @@ export default function SplashscreenEei() {
         <div className={styles.introIconContainer}>
           {<StoryIcon />}
           Story
+          <div className={styles.sunContainer}>
+            <svg viewBox="0 0 200 100" preserveAspectRatio="xMidYMin slice">
+              <defs>
+                <radialGradient id="test" cx="50%" cy="0%" r="100%">
+                  <stop
+                    offset="0%"
+                    stopColor="var(--story-eei-splashcolor)"
+                    stopOpacity="0"
+                  />
+                  <stop
+                    offset="50%"
+                    stopColor="var(--story-eei-splashcolor)"
+                    stopOpacity="0"
+                  />
+                  <stop
+                    offset="70.9%"
+                    stopColor="var(--atmosphere-color)"
+                    stopOpacity="1"
+                  />
+                  <stop offset="71%" stopColor="white" stopOpacity="0.5" />
+                  <stop offset="71.1%" stopColor="white" stopOpacity="0.5" />
+                  <stop
+                    offset="71.2%"
+                    stopColor="var(--atmosphere-color)"
+                    stopOpacity="0.55"
+                  />
+                  <stop
+                    offset="80%"
+                    stopColor="var(--neutral-esa-background)"
+                    stopOpacity="0.25"
+                  />
+                  <stop
+                    offset="100%"
+                    stopColor="var(--neutral-esa-background)"
+                    stopOpacity="1"
+                  />
+                </radialGradient>
+
+                <radialGradient id="test2" cx="50%" cy="0%" r="100%">
+                  <stop
+                    offset="58%"
+                    stopColor="var(--story-eei-splashcolor)"
+                    stopOpacity="1"
+                  />
+                  <stop offset="100%" stopColor="transparent" stopOpacity="1" />
+                </radialGradient>
+              </defs>
+              <rect width="100%" height="100%" fill="url(#test)" />{" "}
+            </svg>
+          </div>
         </div>
-        <div
-
-          className={styles.title}
-
-        >{splashConfig?.title}</div>
+        <div className={styles.title}>{splashConfig?.title}</div>
       </ScrollModule.StickyContainer>
     </ScrollModule>
   );
