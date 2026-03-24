f9e609d3eb52e0f34835ca588aaa8c2a5018ed52
immobe <i.beeck@gmail.com>
2026-03-24 10:22:39 +0100

    fix(mouse-icon): use proper react props

--- src/scripts/components/main/icons/mouse-icon.tsx
+++ src/scripts/components/main/icons/mouse-icon.tsx
@@ -10,8 +10,8 @@ export const MouseIcon: FunctionComponent = () => (
     rotate="-90deg"
   >
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M6 5.48276C6 3.55277 7.57354 2 9.5 2L14.5 2C16.4265 2 18 3.55277 18 5.48276L18 18.5172C18 20.4472 16.4265 22 14.5 22L9.5 22C7.57354 22 6 20.4472 6 18.5172L6 5.48276ZM9.5 4C8.66504 4 8 4.67037 8 5.48276L8 18.5172C8 19.3296 8.66504 20 9.5 20L14.5 20C15.335 20 16 19.3296 16 18.5172L16 5.48276C16 4.67037 15.335 4 14.5 4L9.5 4Z"
       fill="currentColor"
     />
