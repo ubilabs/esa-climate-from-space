e214c01b72a5d022a5c15bb3345c93c05dd16c30
immobe <i.beeck@gmail.com>
2026-03-24 10:45:36 +0100

    feat(globe-caption): add markdown parsing

--- src/scripts/components/layers/globe-caption/globe-caption.tsx
+++ src/scripts/components/layers/globe-caption/globe-caption.tsx
@@ -1,12 +1,28 @@
 import { FunctionComponent } from "react";
+import { StoryMarkdown } from "../../shared/story-markdown/story-markdown";
+import rehypeRaw from "rehype-raw";
+
+import config from "../../../config/main";
+
 import styles from "./globe-caption.module.css";
 
 interface Props {
   caption: string;
+  storyId?: string;
 }
 
-const GlobeCaption: FunctionComponent<Props> = ({ caption }) => {
-  return <div className={styles.globeCaption}>{caption}</div>;
+const GlobeCaption: FunctionComponent<Props> = ({ caption, storyId }) => {
+  return (
+    <div className={styles.globeCaption}>
+      <StoryMarkdown
+        storyId={storyId}
+        rehypePlugins={[rehypeRaw]}
+        allowedElements={config.markdownAllowedElements}
+      >
+        {caption}
+      </StoryMarkdown>
+    </div>
+  );
 };
 
 export default GlobeCaption;
