33ffe9d93f27951386e0e3ea1376f9992bdf8c2f
immobe <i.beeck@gmail.com>
2026-03-25 12:19:02 +0100

    fix(treemap): fix container misalignment

--- src/scripts/hooks/use-screen-info.ts
+++ src/scripts/hooks/use-screen-info.ts
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
 import debounce from "lodash.debounce";
 
-const MOBILE_WIDTH = 768;
-const DESKTOP_WIDTH = 1024;
+// when changing these values, make sure they are aligned with the @custom-media definitions in variables.css
+const MOBILE_WIDTH = 767;
 
 // debounce the time update
 const DELAY = 500;
@@ -12,10 +12,9 @@ export function useScreenInfo() {
     screenHeight: Math.floor(window.innerHeight),
     screenWidth: Math.floor(window.innerWidth),
   });
-  const [isMobile, setIsMobile] = useState(window.innerWidth < MOBILE_WIDTH);
-  const [isDesktop, setIsDesktop] = useState(
-    window.innerWidth >= DESKTOP_WIDTH,
-  );
+
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= MOBILE_WIDTH);
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth > MOBILE_WIDTH);
 
   const isTouchDevice =
     "ontouchstart" in window || navigator.maxTouchPoints > 0;
@@ -27,8 +26,8 @@ export function useScreenInfo() {
         screenWidth: Math.floor(window.innerWidth),
       });
       // Make sure this is the same value as defined in in variables.css
-      setIsMobile(window.innerWidth < MOBILE_WIDTH);
-      setIsDesktop(window.innerWidth >= DESKTOP_WIDTH);
+      setIsMobile(window.innerWidth <= MOBILE_WIDTH);
+      setIsDesktop(window.innerWidth > MOBILE_WIDTH);
     }, DELAY);
 
     window.addEventListener("resize", handleResize);
