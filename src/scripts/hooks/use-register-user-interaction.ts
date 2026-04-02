e748304a028f5ff09982a0757b43156fe458ea2b
immobe <i.beeck@gmail.com>
2026-04-02 14:45:50 +0200

    feat(use-register-user-interaction): add arrow keys and touchend listener

--- src/scripts/hooks/use-register-user-interaction.ts
+++ src/scripts/hooks/use-register-user-interaction.ts
@@ -10,12 +10,24 @@ export const useRegisterUserInteraction = (): boolean => {
       setHasInteracted(true);
     };
 
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (
+        ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].includes(event.key)
+      ) {
+        handleInteraction();
+      }
+    };
+
     window.addEventListener("wheel", handleInteraction);
+    window.addEventListener("touchend", handleInteraction);
     window.addEventListener("click", handleInteraction);
+    window.addEventListener("keydown", handleKeyDown);
 
     return () => {
       window.removeEventListener("wheel", handleInteraction);
+      window.removeEventListener("touchend", handleInteraction);
       window.removeEventListener("click", handleInteraction);
+      window.removeEventListener("keydown", handleKeyDown);
     };
   }, [hasInteracted]);
 
