ffc079c64854bf9e6e3f1f112bee6b68c854fcb3
immobe <i.beeck@gmail.com>
2026-08-12 12:21:03 +0200

    refactor(scroll-story): refactor layer select to hook

--- src/scripts/components/stories/story/scroll-story/story-x-fires/story-x-fires.tsx
+++ src/scripts/components/stories/story/scroll-story/story-x-fires/story-x-fires.tsx
@@ -1,6 +1,9 @@
 import { FunctionComponent } from "react";
 
+import { useStoryLayer } from "../../../../../hooks/use-story-layer";
+
 import Story from "../../story";
+import { Layers } from "./constants/globe";
 import RadiativePowerThresholdModule from "./modules/radiative-power-threshold/radiative-power-threshold";
 import BurnedAreaModule from "./modules/burned-area/burned-area";
 import IntroModule from "./modules/intro/intro-module";
@@ -14,6 +17,8 @@ export type StoryXFiresCompoundComponents = {
 /* Module Wrapper for xFires Story Components */
 export const StoryXFires: FunctionComponent &
   StoryXFiresCompoundComponents = () => {
+  useStoryLayer(Layers.XFIRES_GLOBAL_FIRES, Object.values(Layers));
+
   return <Story />;
 };
 
