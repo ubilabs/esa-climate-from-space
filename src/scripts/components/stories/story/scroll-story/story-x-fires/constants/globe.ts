feae5cc93eda35c92d61d17bc54c9a1ba77b3343
Andreas Helms <helms@ubilabs.net>
2026-08-18 14:52:43 +0200

    feat(x-fires): add x-fires basemap and update existing layers (#2466)
    Co-authored-by: Andreas Helms <helms@ubilabs.com>

--- src/scripts/components/stories/story/scroll-story/story-x-fires/constants/globe.ts
+++ src/scripts/components/stories/story/scroll-story/story-x-fires/constants/globe.ts
@@ -1,5 +1,4 @@
 export const Layers = {
   XFIRES_GLOBAL_FIRES: "x-fires_global_fires",
-  XFIRES_EARTH_MASK: "x-fires_earth_mask",
-  XFIRES_EARTH_MASK_PORTUGAL: "x-fires_earth_mask_portugal",
+  XFIRES_PORTUGAL: "x-fires_portugal",
 };
