9f55829431768a058b5e7645faefae6b24c66620
Andreas Helms <helms@ubilabs.com>
2026-09-09 18:27:16 +0200

    fix(eei-story): layer loading delay on mobile

--- src/scripts/hooks/use-story-layer.ts
+++ src/scripts/hooks/use-story-layer.ts
@@ -1,20 +1,26 @@
 import { useEffect, useEffectEvent } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useThunkDispatch } from "./use-thunk-dispatch";
 
 import { setSelectedLayerIds } from "../reducers/layers";
 import { selectedLayerIdsSelector } from "../selectors/layers/selected-ids";
+import { layersApi } from "../services/api";
 
 export const useStoryLayer = (
   initialLayerId: string,
   storyLayerIds: readonly string[],
 ) => {
   const dispatch = useDispatch();
+  const thunkDispatch = useThunkDispatch();
   const { mainId } = useSelector(selectedLayerIdsSelector);
 
-  const selectStoryLayer = useEffectEvent(() => {
-    dispatch(
-      setSelectedLayerIds({ layerId: initialLayerId, isPrimary: true }),
-    );
+  const initStoryLayer = useEffectEvent(() => {
+    // Fetch the layer details for all layers in the story to prevent loading delays.
+    storyLayerIds.forEach((layerId) => {
+      thunkDispatch(layersApi.endpoints.getLayer.initiate(layerId));
+    });
+
+    dispatch(setSelectedLayerIds({ layerId: initialLayerId, isPrimary: true }));
 
     return () => {
       if (mainId && storyLayerIds.includes(mainId)) {
@@ -23,5 +29,5 @@ export const useStoryLayer = (
     };
   });
 
-  useEffect(() => selectStoryLayer(), []);
+  useEffect(() => initStoryLayer(), []);
 };
