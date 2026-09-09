62322965c234a2fb36ab220ed1bd18a088d1225f
immobe <i.beeck@gmail.com>
2026-09-09 16:36:56 +0200

    feat(shot-3): add some margin to the left

--- src/scripts/components/stories/story/scroll-story/story-x-fires/modules/name-the-beast/naming-the-beast.tsx
+++ src/scripts/components/stories/story/scroll-story/story-x-fires/modules/name-the-beast/naming-the-beast.tsx
@@ -6,6 +6,9 @@ import { useRef } from "react";
 import ScrollModule from "../../../modules/base-scroll/module/scroll-module";
 import GlobalFires from "./global-fires/global-fires";
 import ScrollText from "../../../modules/base-scroll/scroll-text/scroll-text";
+
+import styles from "./naming-the-beast.module.css"
+
 import {
   ENTERING_TEXT_OUTPUT,
   THREE_TEXT_TIMING,
@@ -49,6 +52,7 @@ export default function NamingTheBeast() {
       <ScrollModule.StickyContainer isGrid ref={getRefCallback(0, 0)}>
         <GlobalFires />
         <ScrollText
+          className={styles.beastScrollText}
           inlinePlacement="left"
           isHeadline
           text={xFiresModule.content?.scrollText1 || ""}
@@ -56,12 +60,14 @@ export default function NamingTheBeast() {
           outputRange={animationConfig.scrollText1.output}
         />
         <ScrollText
+          className={styles.beastScrollText}
           inlinePlacement="left"
           text={xFiresModule.content?.scrollText2 || ""}
           inputRange={animationConfig.scrollText2.input}
           outputRange={animationConfig.scrollText2.output}
         />
         <ScrollText
+          className={styles.beastScrollText}
           inlinePlacement="left"
           text={xFiresModule.content?.scrollText3 || ""}
           inputRange={animationConfig.scrollText3.input}
